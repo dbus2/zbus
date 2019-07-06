b06db0f6ea9f44fa6866d3a00c9228ff12869ce0
Zeeshan Ali <zeeshanak@gnome.org>
2019-07-06 23:47:38 +0200

    Simplify VariantType implementation for ObjectPath

--- src/variant_type.rs
+++ src/variant_type.rs
@@ -110,46 +110,23 @@ impl<'a> VariantType<'a> for &'a str {
 
 pub struct ObjectPath<'a>(pub &'a str);
 
-// FIXME: Find a way to share code with &str implementation above
 impl<'a> VariantType<'a> for ObjectPath<'a> {
     const SIGNATURE: char = 'o';
     const SIGNATURE_STR: &'static str = "o";
 
     fn encode(&self) -> Vec<u8> {
-        let len = self.0.len();
-        let mut bytes = Vec::with_capacity(5 + len);
-
-        bytes.extend(&(len as u32).to_ne_bytes());
-        bytes.extend(self.0.as_bytes());
-        bytes.push(b'\0');
-
-        bytes
+        self.0.encode()
     }
 
     fn extract_slice(bytes: &'a [u8]) -> Result<&'a [u8], VariantError> {
-        if bytes.len() < 4 {
-            return Err(VariantError::InsufficientData);
-        }
-
-        let last_index = byteorder::NativeEndian::read_u32(bytes) as usize + 5;
-        if bytes.len() < last_index {
-            return Err(VariantError::InsufficientData);
-        }
-
-        Ok(&bytes[0..last_index])
+        <(&str)>::extract_slice(bytes)
     }
 
     fn extract(bytes: &'a [u8]) -> Result<Self, VariantError>
     where
         Self: 'a,
     {
-        if bytes.len() < 4 {
-            return Err(VariantError::InsufficientData);
-        }
-
-        str::from_utf8(&bytes[4..])
-            .map(|s| Self(s))
-            .map_err(|_| VariantError::InvalidUtf8)
+        <(&str)>::extract(bytes).map(|s| Self(s))
     }
 }
 
