d3b7ae10f51a97ed961f8eb2ff01700819315d9a
Zeeshan Ali <zeeshanak@gnome.org>
2020-02-10 21:37:08 +0100

    Impl VariantValue for Map types
    BTreeMap and HashMap.

--- serde/src/variant_value.rs
+++ serde/src/variant_value.rs
@@ -93,4 +93,28 @@ tuple_impls! {
     16 => (0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6 7 T7 8 T8 9 T9 10 T10 11 T11 12 T12 13 T13 14 T14 15 T15)
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
+use std::collections::{BTreeMap, HashMap};
+use std::hash::{BuildHasher, Hash};
+
+macro_rules! map_impl {
+    ($ty:ident < K $(: $kbound1:ident $(+ $kbound2:ident)*)*, V $(, $typaram:ident : $bound:ident)* >) => {
+        impl<K, V $(, $typaram)*> VariantValue for $ty<K, V $(, $typaram)*>
+        where
+            K: VariantValue $(+ $kbound1 $(+ $kbound2)*)*,
+            V: VariantValue,
+            $($typaram: $bound,)*
+        {
+            #[inline]
+            fn signature() -> Signature<'static> {
+                Signature::from(format!("a{{{}{}}}", K::signature().as_str(), V::signature().as_str()))
+            }
+        }
+    }
+}
+
+map_impl!(BTreeMap<K: Ord, V>);
+map_impl!(HashMap<K: Eq + Hash, V, H: BuildHasher>);
+
 // TODO: Blanket implementation for more types: https://github.com/serde-rs/serde/blob/master/serde/src/ser/impls.rs
