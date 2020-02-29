230f26bf40deb32fd54849c7ee302554d5799eb1
Zeeshan Ali <zeeshanak@gnome.org>
2020-02-29 18:51:30 +0100

    tests: Minor simplification of code

--- serde/src/lib.rs
+++ serde/src/lib.rs
@@ -50,149 +50,149 @@ mod tests {
     use byteorder::{BigEndian as BE, ByteOrder, LittleEndian as LE};
 
     use crate::to_bytes;
-    use crate::{Array, Dict, EncodingFormat};
+    use crate::{Array, Dict, EncodingFormat as Format};
     use crate::{FromVariant, IntoVariant, Variant, VariantValue};
     use crate::{ObjectPath, Signature};
 
     #[test]
     fn u8_variant() {
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &77_u8).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &77_u8).unwrap();
         assert!(encoded.len() == 1);
 
         // As Variant
         let v = 77_u8.into_variant();
         assert!(v.value_signature() == "y");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &v).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &v).unwrap();
         assert!(encoded.len() == 4);
     }
 
     #[test]
     fn u16_variant() {
-        let encoded = to_bytes::<_, BE>(EncodingFormat::DBus, &0xABBA_u16).unwrap();
+        let encoded = to_bytes::<_, BE>(Format::DBus, &0xABBA_u16).unwrap();
         assert!(encoded.len() == 2);
         assert!(LE::read_u16(&encoded) == 0xBAAB_u16);
 
         // As Variant
         let v = 0xFEFE_u16.into_variant();
         assert!(v.value_signature() == "q");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &v).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &v).unwrap();
         assert!(encoded.len() == 6);
     }
 
     #[test]
     fn i16_variant() {
-        let encoded = to_bytes::<_, BE>(EncodingFormat::DBus, &-0xAB0_i16).unwrap();
+        let encoded = to_bytes::<_, BE>(Format::DBus, &-0xAB0_i16).unwrap();
         assert!(encoded.len() == 2);
         assert!(LE::read_i16(&encoded) == 0x50F5_i16);
 
         // As Variant
         let v = 0xAB_i16.into_variant();
         assert!(v.value_signature() == "n");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &v).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &v).unwrap();
         assert!(encoded.len() == 6);
     }
 
     #[test]
     fn u32_variant() {
-        let encoded = to_bytes::<_, BE>(EncodingFormat::DBus, &0xABBA_ABBA_u32).unwrap();
+        let encoded = to_bytes::<_, BE>(Format::DBus, &0xABBA_ABBA_u32).unwrap();
         assert!(encoded.len() == 4);
         assert!(LE::read_u32(&encoded) == 0xBAAB_BAAB_u32);
 
         // As Variant
         let v = 0xABBA_ABBA_u32.into_variant();
         assert!(v.value_signature() == "u");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &v).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &v).unwrap();
         assert!(encoded.len() == 8);
     }
 
     #[test]
     fn i32_variant() {
-        let encoded = to_bytes::<_, BE>(EncodingFormat::DBus, &-0xABBA_AB0_i32).unwrap();
+        let encoded = to_bytes::<_, BE>(Format::DBus, &-0xABBA_AB0_i32).unwrap();
         assert!(encoded.len() == 4);
         assert!(LE::read_i32(&encoded) == 0x5055_44F5_i32);
 
         // As Variant
         let v = 0xABBA_AB0_i32.into_variant();
         assert!(v.value_signature() == "i");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &v).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &v).unwrap();
         assert!(encoded.len() == 8);
     }
 
     #[test]
     fn u64_variant() {
-        let encoded = to_bytes::<_, BE>(EncodingFormat::DBus, &0xABBA_ABBA_ABBA_ABBA_u64).unwrap();
+        let encoded = to_bytes::<_, BE>(Format::DBus, &0xABBA_ABBA_ABBA_ABBA_u64).unwrap();
         assert!(encoded.len() == 8);
         assert!(LE::read_u64(&encoded) == 0xBAAB_BAAB_BAAB_BAAB_u64);
 
         // As Variant
         let v = 0xFEFE_u64.into_variant();
         assert!(v.value_signature() == "t");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &v).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &v).unwrap();
         assert!(encoded.len() == 16);
     }
 
     #[test]
     fn i64_variant() {
-        let encoded = to_bytes::<_, BE>(EncodingFormat::DBus, &-0xABBA_ABBA_ABBA_AB0_i64).unwrap();
+        let encoded = to_bytes::<_, BE>(Format::DBus, &-0xABBA_ABBA_ABBA_AB0_i64).unwrap();
         assert!(encoded.len() == 8);
         assert!(LE::read_i64(&encoded) == 0x5055_4455_4455_44F5_i64);
 
         // As Variant
         let v = 0xABBA_AB0i64.into_variant();
         assert!(v.value_signature() == "x");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &v).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &v).unwrap();
         assert!(encoded.len() == 16);
     }
 
     #[test]
     fn f64_variant() {
-        let encoded = to_bytes::<_, BE>(EncodingFormat::DBus, &99999.99999_f64).unwrap();
+        let encoded = to_bytes::<_, BE>(Format::DBus, &99999.99999_f64).unwrap();
         assert!(encoded.len() == 8);
         assert!(LE::read_f64(&encoded) == -5759340900185448e-143);
 
         // As Variant
         let v = 99999.99999_f64.into_variant();
         assert!(v.value_signature() == "d");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &v).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &v).unwrap();
         assert!(encoded.len() == 16);
     }
 
     #[test]
     fn str_variant() {
         let string = "hello world";
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &string).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &string).unwrap();
         assert!(encoded.len() == 16);
 
         // As Variant
         let v = string.into_variant();
         assert!(v.value_signature() == "s");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &v).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &v).unwrap();
         assert!(encoded.len() == 20);
     }
 
     #[test]
     fn signature_variant() {
         let sig = Signature::from("yys");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &sig).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &sig).unwrap();
         assert!(encoded.len() == 5);
 
         // As Variant
         let v = sig.into_variant();
         assert!(v.value_signature() == "g");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &v).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &v).unwrap();
         assert!(encoded.len() == 8);
     }
 
     #[test]
     fn object_path_variant() {
         let o = ObjectPath::from("/hello/world");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &o).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &o).unwrap();
         assert!(encoded.len() == 17);
 
         // As Variant
         let v = o.into_variant();
         assert!(v.value_signature() == "o");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &v).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &v).unwrap();
         assert!(encoded.len() == 21);
     }
 
@@ -206,29 +206,29 @@ mod tests {
         let ay = [77u8, 88];
         // Array itself is treated like a tuple by serde & that translates to a structure in our
         // case so gotta make it a slice for serde to treat it as seq type.
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &ay[..]).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &ay[..]).unwrap();
         assert!(encoded.len() == 6);
 
         // As Variant
         let v = &ay[..].into_variant();
         assert!(v.value_signature() == "ay");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, v).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, v).unwrap();
         assert!(encoded.len() == 10);
 
         // Now try as Vec
         let vec = ay.to_vec();
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &vec).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &vec).unwrap();
         assert!(encoded.len() == 6);
 
         // Vec as Variant
         let v = Array::from(&vec).into_variant();
         assert!(v.value_signature() == "ay");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &v).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &v).unwrap();
         assert!(encoded.len() == 10);
 
         // Emtpy array
         let ay: [u64; 0] = [];
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &ay[..]).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &ay[..]).unwrap();
         assert!(encoded.len() == 8);
 
         //
@@ -236,13 +236,13 @@ mod tests {
         //
         // Can't use 'as' as it's a keyword
         let as_ = ["Hello", "World", "Now", "Bye!"];
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &as_[..]).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &as_[..]).unwrap();
         assert!(encoded.len() == 45);
 
         // As Variant
         let v = &as_[..].into_variant();
         assert!(v.value_signature() == "as");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, v).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, v).unwrap();
         assert!(encoded.len() == 49);
 
         // Array of Struct, which in turn containin an Array (We gotta go deeper!)
@@ -262,13 +262,13 @@ mod tests {
             // one more top-most simple field
             "hello",
         )];
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &ar[..]).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &ar[..]).unwrap();
         assert!(encoded.len() == 78);
 
         // As Variant
         let v = &ar[..].into_variant();
         assert!(v.value_signature() == "a(yu(xbxas)s)");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, v).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, v).unwrap();
         assert!(dbg!(encoded.len()) == 94);
     }
 
@@ -277,13 +277,13 @@ mod tests {
         let mut map: HashMap<i64, &str> = HashMap::new();
         map.insert(1, "123");
         map.insert(2, "456");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &map).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &map).unwrap();
         assert!(dbg!(encoded.len()) == 40);
 
         // As Variant
         let v = Dict::from(map).into_variant();
         assert!(v.value_signature() == "a{xs}");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &v).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &v).unwrap();
         assert!(encoded.len() == 48);
         // Convert it back
         let dict = Dict::from_variant(v).unwrap();
@@ -297,7 +297,7 @@ mod tests {
         dict.add("bye", "now".into_variant()).unwrap();
         let v = dict.into_variant();
         assert!(v.value_signature() == "a{sv}");
-        let encoded = to_bytes::<_, LE>(EncodingFormat::DBus, &v).unwrap();
+        let encoded = to_bytes::<_, LE>(Format::DBus, &v).unwrap();
         assert!(dbg!(encoded.len()) == 68);
     }
 }
