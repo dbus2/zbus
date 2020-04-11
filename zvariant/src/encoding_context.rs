357fda71d69eff62ae438cee8ee61804c20aa731
Zeeshan Ali <zeeshanak@gnome.org>
2020-04-11 21:29:50 +0200

    zv: Add n_bytes_before to EncodingContext
    Currently the serializer and deserializer assume 8-byte alignment and
    that assumption isn't always correct as e.g we could be deserializing a
    specific value from large message.

--- zvariant/src/encoding_context.rs
+++ zvariant/src/encoding_context.rs
@@ -18,14 +18,26 @@ impl Default for EncodingFormat {
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub struct EncodingContext {
     format: EncodingFormat,
+    n_bytes_before: usize,
 }
 
 impl EncodingContext {
     pub fn new(format: EncodingFormat) -> Self {
-        Self { format }
+        Self::new_n_bytes_before(format, 0)
+    }
+
+    pub fn new_n_bytes_before(format: EncodingFormat, n_bytes_before: usize) -> Self {
+        Self {
+            format,
+            n_bytes_before,
+        }
     }
 
     pub fn format(self) -> EncodingFormat {
         self.format
+    }
+
+    pub fn n_bytes_before(self) -> usize {
+        self.n_bytes_before
     }
 }
