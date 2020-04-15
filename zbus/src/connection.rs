dbus2/zbus

commit 9f299eedcbe1c28ed490b236a1cb28acdad4e88f

Port zbus to new serde-based zvariant API
It works!!! Also "seems" much faster and definitely simplifies the code.

zbus/src/connection.rs
@@ -4,12 +4,11 @@ use std::{env, error, fmt, io};
 
 use nix::unistd::Uid;
 
-use zvariant::VariantError;
-use zvariant::{Decode, Encode};
+use zvariant::{Error as VariantError, FromVariant};
 
 use crate::address::{self, Address, AddressError};
 use crate::message;
-use crate::message_field;
+use crate::message_field::{self, MessageFieldCode};
 
 pub struct Connection {
     pub server_guid: String,
@@ -110,34 +109,26 @@ impl From<message::Message> for ConnectionError {
         match message.fields() {
             Ok(all_fields) => {
                 // First, get the error name
-                let name = match all_fields.iter().find(|f| {
-                    f.code()
-                        .map(|c| c == message_field::MessageFieldCode::ErrorName)
-                        .unwrap_or(false)
-                }) {
-                    Some(f) => match f.value() {
-                        Ok(v) => match String::from_variant(v) {
-                            Ok(s) => String::from(s),
-                            Err(e) => return ConnectionError::Variant(e),
-                        },
-                        Err(e) => return ConnectionError::MessageField(e),
+                let name = match all_fields
+                    .iter()
+                    .find(|f| f.code() == MessageFieldCode::ErrorName)
+                {
+                    Some(f) => match String::from_variant_ref(f.value()) {
+                        Ok(s) => String::from(s),
+                        Err(e) => return ConnectionError::Variant(e),
                     },
                     None => return ConnectionError::InvalidReply,
                 };
 
                 // Then, try to get the optional description string
-                if message.body_len() > 0 {
-                    match message.body(Some(<String>::SIGNATURE_STR.into())) {
-                        Ok(body) => match String::from_variant(&body.fields()[0]) {
-                            Ok(detail) => {
-                                ConnectionError::MethodError(name, Some(String::from(detail)))
-                            }
-                            Err(e) => ConnectionError::Variant(e),
-                        },
-                        Err(e) => ConnectionError::Message(e),
-                    }
-                } else {
-                    ConnectionError::MethodError(name, None)
+                match message.body::<&str>() {
+                    Ok(body) => match body {
+                        Some(detail) => {
+                            ConnectionError::MethodError(name, Some(String::from(detail)))
+                        }
+                        None => ConnectionError::MethodError(name, None),
+                    },
+                    Err(e) => ConnectionError::Message(e),
                 }
             }
             Err(e) => ConnectionError::Message(e),
@@ -197,7 +188,7 @@ impl Connection {
         };
 
         // Now that daemon has approved us, we must send a hello as per specs
-        let reply = connection.call_method(
+        let reply = connection.call_method::<()>(
             Some("org.freedesktop.DBus"),
             "/org/freedesktop/DBus",
             Some("org.freedesktop.DBus"),
@@ -205,22 +196,25 @@ impl Connection {
             None,
         )?;
 
-        let body = reply.body(Some(<String>::SIGNATURE_STR.into()))?;
-        let bus_name = String::from_variant(&body.fields()[0])?;
-
+        let bus_name = reply
+            .body::<&str>()?
+            .ok_or_else(|| ConnectionError::InvalidReply)?;
         println!("bus name: {}", bus_name);
 
         Ok(connection)
     }
 
-    pub fn call_method(
+    pub fn call_method<B>(
         &mut self,
         destination: Option<&str>,
         path: &str,
         iface: Option<&str>,
         method_name: &str,
-        body: Option<zvariant::Structure>,
-    ) -> Result<message::Message, ConnectionError> {
+        body: Option<&B>,
+    ) -> Result<message::Message, ConnectionError>
+    where
+        B: serde::ser::Serialize + zvariant::VariantValue,
+    {
         println!("Starting: {}", method_name);
         let serial = self.next_serial();
         let m = message::Message::method(destination, path, iface, method_name, body)?
@@ -249,12 +243,8 @@ impl Connection {
                 let all_fields = incoming.fields()?;
 
                 let find_serial_func = |f: &crate::MessageField| {
-                    f.code()
-                        .map(|c| c == message_field::MessageFieldCode::ReplySerial)
-                        .unwrap_or(false)
-                        && f.value()
-                            .map(|v| u32::from_variant(v).map(|u| *u == serial).unwrap_or(false))
-                            .unwrap()
+                    f.code() == MessageFieldCode::ReplySerial
+                        && *f.value() == zvariant::Variant::U32(serial)
                 };
 
                 if all_fields.iter().any(find_serial_func) {

Cargo.toml
@@ -1,3 +1,2 @@
 [workspace]
-members = ["serde"]
-#members = ["zbus", "serde"]
+members = ["zbus", "serde"]

zbus/Cargo.toml
@@ -15,4 +15,7 @@ readme = "../README.md"
 [dependencies]
 byteorder = "1.3.1"
 nix = "0.13.0"
-zvariant = { path = "../zvariant", version = "^2" }
+serde = "1.0"
+serde_derive = "1.0"
+#arrayvec = { version = "0.5.1", features = [ "serde" ] }
+zvariant = { path = "../serde", version = "^2" }

zbus/src/lib.rs
@@ -17,34 +17,20 @@ mod utils;
 
 #[cfg(test)]
 mod tests {
-    use core::convert::{TryFrom, TryInto};
     use std::collections::HashMap;
 
-    use zvariant::{Array, Decode, Dict, Encode};
-    use zvariant::{Structure, Variant};
+    use zvariant::{Array, FromVariant};
+    use zvariant::{Variant, VariantValue};
 
     #[test]
     fn basic_connection() {
-        let mut connection = crate::Connection::new_session()
+        crate::Connection::new_session()
             .map_err(|e| {
                 println!("error: {}", e);
 
                 e
             })
             .unwrap();
-
-        // Hello method is already called during connection creation so subsequent calls are expected to fail but only
-        // with a D-Bus error.
-        match connection.call_method(
-            Some("org.freedesktop.DBus"),
-            "/org/freedesktop/DBus",
-            Some("org.freedesktop.DBus"),
-            "Hello",
-            None,
-        ) {
-            Err(crate::ConnectionError::MethodError(_, _)) => (),
-            _ => panic!(),
-        };
     }
 
     #[test]
@@ -59,7 +45,7 @@ mod tests {
 
         if std::env::var("GET_MACHINE_ID").unwrap_or(String::from("1")) == "1" {
             let reply = connection
-                .call_method(
+                .call_method::<()>(
                     Some("org.freedesktop.DBus"),
                     "/org/freedesktop/DBus",
                     Some("org.freedesktop.DBus.Peer"),
@@ -70,10 +56,9 @@ mod tests {
 
             assert!(reply
                 .body_signature()
-                .map(|s| s.as_str() == <String>::SIGNATURE_STR)
+                .map(|s| s == <&str>::signature())
                 .unwrap());
-            let body = reply.body(Some(<String>::SIGNATURE_STR.into())).unwrap();
-            let id = String::from_variant(&body.fields()[0]).unwrap();
+            let id: &str = reply.body().unwrap().unwrap();
             println!("Machine ID: {}", id);
         }
 
@@ -83,16 +68,15 @@ mod tests {
                 "/org/freedesktop/DBus",
                 Some("org.freedesktop.DBus"),
                 "NameHasOwner",
-                Some(Structure::new().add_field(String::from("org.freedesktop.DBus"))),
+                Some(&"org.freedesktop.DBus"),
             )
             .unwrap();
 
         assert!(reply
             .body_signature()
-            .map(|s| s.as_str() == bool::SIGNATURE_STR)
+            .map(|s| s == bool::signature())
             .unwrap());
-        let body = reply.body(Some(bool::SIGNATURE_STR.into())).unwrap();
-        assert!(bool::from_variant(&body.fields()[0]).unwrap());
+        assert!(reply.body::<bool>().unwrap().unwrap());
 
         let reply = connection
             .call_method(
@@ -100,16 +84,15 @@ mod tests {
                 "/org/freedesktop/DBus",
                 Some("org.freedesktop.DBus"),
                 "GetNameOwner",
-                Some(Structure::new().add_field(String::from("org.freedesktop.DBus"))),
+                Some(&"org.freedesktop.DBus"),
             )
             .unwrap();
 
         assert!(reply
             .body_signature()
-            .map(|s| s.as_str() == <String>::SIGNATURE_STR)
+            .map(|s| s == <&str>::signature())
             .unwrap());
-        let body = reply.body(None).unwrap();
-        let owner = String::from_variant(&body.fields()[0]).unwrap();
+        let owner: &str = reply.body().unwrap().unwrap();
         println!("Owner of 'org.freedesktop.DBus' is: {}", owner);
 
         let reply = connection
@@ -118,7 +101,7 @@ mod tests {
                 "/org/freedesktop/DBus",
                 Some("org.freedesktop.DBus.Properties"),
                 "GetAll",
-                Some(Structure::new().add_field(String::from("org.freedesktop.DBus"))),
+                Some(&"org.freedesktop.DBus"),
             )
             .unwrap();
 
@@ -126,32 +109,21 @@ mod tests {
             .body_signature()
             .map(|s| s.as_str() == "a{sv}")
             .unwrap());
-        let body = reply.body(Some("a{sv}".into())).unwrap();
-        let mut fields = body.take_fields();
-        let array = Array::take_from_variant(fields.remove(0)).unwrap();
-        let dict = Dict::try_from(array).unwrap();
-        let mut hashmap: HashMap<String, Variant> = dict.try_into().unwrap();
+        let hashmap: HashMap<&str, Variant> = reply.body().unwrap().unwrap();
 
         // "Features" property
-        let features: Vec<String> = Array::take_from_variant(hashmap.remove("Features").unwrap())
-            .unwrap()
-            .try_into()
-            .unwrap();
+        let features = Array::from_variant_ref(&hashmap["Features"]).unwrap();
         println!("org.freedesktop.DBus.Features on /org/freedesktop/DBus:");
-        for feature in features {
-            print!(" {}", feature);
+        for feature in features.get() {
+            print!(" {}", <&str>::from_variant_ref(feature).unwrap());
         }
         println!();
 
         // "Interfaces" property
-        let interfaces: Vec<String> =
-            Array::take_from_variant(hashmap.remove("Interfaces").unwrap())
-                .unwrap()
-                .try_into()
-                .unwrap();
+        let interfaces = Array::from_variant_ref(&hashmap["Interfaces"]).unwrap();
         println!("org.freedesktop.DBus.Interfaces on /org/freedesktop/DBus:");
-        for interface in interfaces {
-            print!(" {}", interface);
+        for interface in interfaces.get() {
+            print!(" {}", <&str>::from_variant_ref(interface).unwrap());
         }
         println!();
     }

zbus/src/message.rs
@@ -1,13 +1,10 @@
 use byteorder::ByteOrder;
-use core::convert::TryInto;
 use std::error;
 use std::fmt;
-use std::io::Error as IOError;
+use std::io::{Error as IOError, Write};
 
-use zvariant::Signature;
-use zvariant::VariantError;
-use zvariant::{Array, Decode, Encode, EncodingFormat};
-use zvariant::{SharedData, Structure};
+use zvariant::{EncodingFormat, Error as VariantError, FromVariant};
+use zvariant::{Signature, VariantValue};
 
 use crate::utils::padding_for_8_bytes;
 use crate::{MessageField, MessageFieldCode, MessageFieldError, MessageFields};
@@ -111,15 +108,22 @@ impl From<IOError> for MessageError {
 #[derive(Debug)]
 pub struct Message(Vec<u8>);
 
+// TODO: Make generic over byteorder
+// TODO: Document multiple args needing to be a tuple
 impl Message {
-    pub fn method(
+    pub fn method<B>(
         destination: Option<&str>,
         path: &str,
         iface: Option<&str>,
         method_name: &str,
-        body: Option<Structure>,
-    ) -> Result<Self, MessageError> {
-        let mut m = Message::new(MessageType::MethodCall);
+        // TODO: callback for caller to serialize directly into the message?
+        body: Option<&B>,
+    ) -> Result<Self, MessageError>
+    where
+        B: serde::ser::Serialize + VariantValue,
+    {
+        let mut bytes: Vec<u8> = Vec::with_capacity(PRIMARY_HEADER_SIZE);
+        let mut cursor = std::io::Cursor::new(&mut bytes);
 
         let dest_length = destination.map_or(0, |s| s.len());
         let iface_length = iface.map_or(0, |s| s.len());
@@ -133,14 +137,7 @@ impl Message {
             return Err(MessageError::StrTooLarge);
         }
 
-        // Message body encoding set to 0 at first
-        let body_len_position = m.0.len();
-        m.0.extend(&0u32.to_ne_bytes());
-
-        // Serial number. FIXME: managed by connection
-        m.0.extend(&1u32.to_ne_bytes());
-
-        // Now array of fields
+        // Construct the array of fields
         let mut fields = MessageFields::new();
 
         if let Some(destination) = destination {
@@ -149,42 +146,54 @@ impl Message {
         if let Some(iface) = iface {
             fields.add(MessageField::interface(iface));
         }
-        let body_signature = body.as_ref().map(|structure| {
-            let signature = structure.signature();
-            // Remove the leading and trailing STRUCT delimiter
-            let signature = &signature[1..signature.len() - 1];
-
-            String::from(signature)
-        });
-        if let Some(body_signature) = body_signature {
-            fields.add(MessageField::signature(body_signature));
+        if body.is_some() {
+            let mut signature = B::signature();
+            // Remove any leading and trailing STRUCT delimiters
+            if signature.starts_with(zvariant::STRUCT_SIG_START_STR) {
+                signature = Signature::from(String::from(&signature[1..signature.len() - 1]));
+            }
+            fields.add(MessageField::signature(signature));
         }
 
         fields.add(MessageField::path(path));
         fields.add(MessageField::member(method_name));
 
         let format = EncodingFormat::DBus;
-        let array = Array::from(fields);
-        array.encode_into(&mut m.0, format);
+        zvariant::to_write_ne(
+            &mut cursor,
+            format,
+            &(
+                ENDIAN_SIG,                    // Endianness
+                MessageType::MethodCall as u8, // Message type
+                0u8,                           // Flags
+                1u8,                           // Major version of D-Bus protocol
+                0u32,                          // Message body encoding set to 0 at first
+                1u32,                          // Serial number. FIXME: managed by connection
+                fields,
+            ),
+        )?; // Array of fields
 
         // Do we need to do this if body is None?
-        let padding = padding_for_8_bytes(m.0.len());
+        let padding = padding_for_8_bytes(cursor.position() as usize);
         if padding > 0 {
-            m.push_padding(padding);
+            for _ in 0..padding {
+                cursor.write_all(&[0u8; 1])?;
+            }
         }
 
         if let Some(body) = body {
-            let n_bytes_before = m.0.len();
-            body.encode_into(&mut m.0, format);
+            let pos = cursor.position();
+            zvariant::to_write_ne(&mut cursor, format, body)?;
 
-            let len = crate::utils::usize_to_u32(m.0.len() - n_bytes_before);
-            byteorder::NativeEndian::write_u32(
-                &mut m.0[body_len_position..body_len_position + 4],
-                len,
-            );
+            let body_len = cursor.position() - pos;
+            if body_len > (u32::max_value() as u64) {
+                return Err(MessageError::ExcessData);
+            }
+            cursor.set_position(BODY_LEN_START_OFFSET as u64);
+            zvariant::to_write_ne(&mut cursor, format, &(body_len as u32))?;
         }
 
-        Ok(m)
+        Ok(Message(bytes))
     }
 
     pub fn set_serial(mut self, serial: u32) -> Self {
@@ -236,11 +245,11 @@ impl Message {
 
     pub fn body_signature(&self) -> Result<Signature, MessageError> {
         for field in self.fields()?.get() {
-            if field.code()? == MessageFieldCode::Signature {
-                let value = field.value()?;
-                let sig = Signature::from_variant(value)?;
+            if field.code() == MessageFieldCode::Signature {
+                let sig = Signature::from_variant_ref(field.value())?;
 
-                return Ok(Signature::from(sig.as_str()));
+                // FIXME: Can we avoid the copy?
+                return Ok(Signature::from(String::from(sig)));
             }
         }
 
@@ -256,17 +265,18 @@ impl Message {
             return Err(MessageError::InsufficientData);
         }
 
-        // FIXME: We can avoid this deep copy (perhaps if we have builder pattern for Message?)
-        let encoding = SharedData::new(self.0.clone());
-        let format = EncodingFormat::DBus;
-        let slice = Array::slice_data(&encoding.tail(FIELDS_LEN_START_OFFSET), "a(yv)", format)?;
-
-        let array = Array::decode(&slice, "a(yv)", format).map_err(MessageError::from)?;
-
-        array.try_into().map_err(MessageError::from)
+        zvariant::from_slice_ne::<(u8, u8, u8, u8, u32, u32, MessageFields)>(
+            &self.0,
+            EncodingFormat::DBus,
+        )
+        .map(|(_, _, _, _, _, _, fields)| fields)
+        .map_err(MessageError::from)
     }
 
-    pub fn body(&self, body_signature: Option<Signature>) -> Result<Structure, MessageError> {
+    pub fn body<'d, 'm: 'd, B>(&'m self) -> Result<Option<B>, MessageError>
+    where
+        B: serde::de::Deserialize<'d> + VariantValue,
+    {
         if self.bytes_to_completion() != 0 {
             return Err(MessageError::InsufficientData);
         }
@@ -274,19 +284,12 @@ impl Message {
         let mut header_len = PRIMARY_HEADER_SIZE + self.fields_len();
         header_len = header_len + padding_for_8_bytes(header_len);
         if self.body_len() == 0 {
-            return Ok(Structure::new());
+            return Ok(None);
         }
 
-        let signature = body_signature.unwrap_or(self.body_signature()?);
-        // Add () for Structure
-        let signature = format!("({})", signature.as_str());
-
-        // FIXME: We can avoid this deep copy (perhaps if we have builder pattern for Message?)
-        let encoding = SharedData::new(self.0.clone());
-        let structure =
-            Structure::decode(&encoding.tail(header_len), signature, EncodingFormat::DBus)?;
-
-        Ok(structure)
+        zvariant::from_slice_ne(&self.0[header_len..], EncodingFormat::DBus)
+            .map(Some)
+            .map_err(MessageError::from)
     }
 
     pub fn no_reply_expected(mut self) -> Self {
@@ -309,18 +312,5 @@ impl Message {
 
     pub fn as_bytes(&self) -> &[u8] {
         &self.0
-    }
-
-    fn new(mtype: MessageType) -> Self {
-        Message(vec![
-            ENDIAN_SIG,  // Endianness
-            mtype as u8, // Message type
-            0,           // Flags
-            1,           // Major version of D-Bus protocol
-        ])
-    }
-
-    fn push_padding(&mut self, len: usize) {
-        self.0.extend(std::iter::repeat(0).take(len));
     }
 }

zbus/src/message_field.rs
@@ -1,13 +1,14 @@
-use core::convert::TryFrom;
-
 use std::error;
 use std::fmt;
 
-use zvariant::{Decode, Encode};
-use zvariant::{ObjectPath, Signature, Structure};
-use zvariant::{Variant, VariantError};
 use serde::de::{Deserialize, Deserializer};
 use serde::ser::{Serialize, Serializer};
+
+use serde_derive::{Deserialize, Serialize};
+
+use zvariant::IntoVariant;
+use zvariant::{Error as VariantError, Variant, VariantValue};
+use zvariant::{ObjectPath, Signature};
 
 #[repr(u8)]
 #[derive(Copy, Clone, Debug, PartialEq)]
@@ -93,134 +94,97 @@ impl From<VariantError> for MessageFieldError {
     }
 }
 
-#[derive(Debug)]
-pub struct MessageField(Structure);
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MessageField<'v> {
+    code: MessageFieldCode,
+    #[serde(borrow)]
+    value: Variant<'v>,
+}
 
-impl MessageField {
-    pub fn code(&self) -> Result<MessageFieldCode, MessageFieldError> {
-        let fields = self.0.fields();
-        if fields.len() < 2 {
-            return Err(MessageFieldError::InsufficientData);
+impl<'v> MessageField<'v> {
+    pub fn code(&self) -> MessageFieldCode {
+        self.code
+    }
+
+    //pub fn value<'d, 'f: 'd, V>(&'f self) -> Result<V, MessageFieldError>
+    //where
+    //    B: serde::de::Deserialize<'d> + VariantValue,
+    //{
+    pub fn value(&self) -> &Variant {
+        &self.value
+    }
+
+    pub fn path<'o: 'v>(path: impl Into<ObjectPath<'o>>) -> Self {
+        Self {
+            code: MessageFieldCode::Path,
+            value: path.into().into_variant(),
         }
-
-        Ok(u8::from_variant(&self.0.fields()[0])
-            .map(|c| MessageFieldCode::from(*c))
-            .unwrap_or(MessageFieldCode::Invalid))
     }
 
-    pub fn value(&self) -> Result<&Variant, MessageFieldError> {
-        Ok(Variant::from_variant(&self.0.fields()[1])?)
+    pub fn interface<'i: 'v>(interface: &'i str) -> Self {
+        Self {
+            code: MessageFieldCode::Interface,
+            value: interface.into_variant(),
+        }
     }
 
-    pub fn get(&self) -> &Structure {
-        &self.0
+    pub fn member<'m: 'v>(member: &'m str) -> Self {
+        Self {
+            code: MessageFieldCode::Member,
+            value: member.into_variant(),
+        }
     }
 
-    pub fn get_mut(&mut self) -> &mut Structure {
-        &mut self.0
-    }
-
-    pub fn into_inner(self) -> Structure {
-        self.0
-    }
-
-    pub fn path(path: impl Into<ObjectPath>) -> Self {
-        Self(
-            Structure::new()
-                .add_field(MessageFieldCode::Path as u8)
-                .add_field(path.into().to_variant()),
-        )
-    }
-
-    pub fn interface(interface: &str) -> Self {
-        Self(
-            Structure::new()
-                .add_field(MessageFieldCode::Interface as u8)
-                .add_field(String::from(interface).to_variant()),
-        )
-    }
-
-    pub fn member(member: &str) -> Self {
-        Self(
-            Structure::new()
-                .add_field(MessageFieldCode::Member as u8)
-                .add_field(String::from(member).to_variant()),
-        )
-    }
-
-    pub fn error_name(error_name: &str) -> Self {
-        Self(
-            Structure::new()
-                .add_field(MessageFieldCode::ErrorName as u8)
-                .add_field(String::from(error_name).to_variant()),
-        )
+    pub fn error_name<'e: 'v>(error_name: &'e str) -> Self {
+        Self {
+            code: MessageFieldCode::ErrorName,
+            value: error_name.into_variant(),
+        }
     }
 
     pub fn reply_serial(serial: u32) -> Self {
-        Self(
-            Structure::new()
-                .add_field(MessageFieldCode::ReplySerial as u8)
-                .add_field(serial.to_variant()),
-        )
+        Self {
+            code: MessageFieldCode::ReplySerial,
+            value: serial.into_variant(),
+        }
     }
 
-    pub fn destination(destination: &str) -> Self {
-        Self(
-            Structure::new()
-                .add_field(MessageFieldCode::Destination as u8)
-                .add_field(String::from(destination).to_variant()),
-        )
+    pub fn destination<'d: 'v>(destination: &'d str) -> Self {
+        Self {
+            code: MessageFieldCode::Destination,
+            value: destination.into_variant(),
+        }
     }
 
-    pub fn sender(sender: &str) -> Self {
-        Self(
-            Structure::new()
-                .add_field(MessageFieldCode::Sender as u8)
-                .add_field(String::from(sender).to_variant()),
-        )
+    pub fn sender<'s: 'v>(sender: &'s str) -> Self {
+        Self {
+            code: MessageFieldCode::Sender,
+            value: sender.into_variant(),
+        }
     }
 
-    pub fn signature(signature: impl Into<Signature>) -> Self {
-        Self(
-            Structure::new()
-                .add_field(MessageFieldCode::Signature as u8)
-                .add_field(signature.into().to_variant()),
-        )
+    pub fn signature<'s: 'v>(signature: impl Into<Signature<'s>>) -> Self {
+        Self {
+            code: MessageFieldCode::Signature,
+            value: signature.into().into_variant(),
+        }
     }
 
     pub fn unix_fds(fd: u32) -> Self {
-        Self(
-            Structure::new()
-                .add_field(MessageFieldCode::UnixFDs as u8)
-                .add_field(fd.to_variant()),
-        )
+        Self {
+            code: MessageFieldCode::UnixFDs,
+            value: fd.into_variant(),
+        }
     }
 }
 
-impl std::ops::Deref for MessageField {
-    type Target = Structure;
-
-    fn deref(&self) -> &Self::Target {
-        self.get()
-    }
-}
-
-impl std::ops::DerefMut for MessageField {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        self.get_mut()
-    }
-}
-
-impl TryFrom<Structure> for MessageField {
-    type Error = MessageFieldError;
-
-    fn try_from(structure: Structure) -> Result<Self, MessageFieldError> {
-        let field = MessageField(structure);
-
-        // Ensure there is a valid code & variant payload
-        let _ = field.code()?;
-        let _ = field.value()?;
-
-        Ok(field)
+// FIXME: Use derive macro when it's available
+impl<'a> VariantValue for MessageField<'a> {
+    fn signature() -> Signature<'static> {
+        Signature::from(format!(
+            "({}{})",
+            u8::signature().as_str(),
+            Variant::signature().as_str(),
+        ))
     }
 }

commit a7e4d363bd049caf4647946a71cbb5ef712b3298

zv,zb: Don't provide FromVariant impl. for String
Cause if we have Variant::Str, it'll be valid to call
String::from_variant_ref() on it and we can't return reference to a value
owned by the function itself.

This fixes the regression from 9f299eedcbe1c28ed490b236a1cb28acdad4e88f
where we stopped being able to parse the error name string from error
replies.

zbus/src/connection.rs
@@ -118,8 +118,8 @@ impl From<Message> for ConnectionError {
             .iter()
             .find(|f| f.code() == MessageFieldCode::ErrorName)
         {
-            Some(f) => match String::from_variant_ref(f.value()) {
-                Ok(s) => String::from(s),
+            Some(f) => match <&str>::from_variant_ref(f.value()) {
+                Ok(s) => String::from(*s),
                 Err(e) => return ConnectionError::Variant(e),
             },
             None => return ConnectionError::InvalidReply,

zvariant/src/from_variant.rs
@@ -211,25 +211,9 @@ impl<'s, 'v: 's> FromVariant<'v> for &'s str {
     }
 }
 
-// String
-
-impl<'v> FromVariant<'v> for String {
-    fn from_variant(variant: Variant<'v>) -> Result<Self, Error> {
-        if let Variant::String(value) = variant {
-            Ok(value)
-        } else {
-            Err(Error::IncorrectType)
-        }
-    }
-
-    fn from_variant_ref(variant: &'v Variant<'v>) -> Result<&'v Self, Error> {
-        if let Variant::String(value) = variant {
-            Ok(value)
-        } else {
-            Err(Error::IncorrectType)
-        }
-    }
-}
+// Not providing FromVariant for String implementtion cause if we have Variant::Str,
+// it'll be valid to call String::from_variant_ref() on it and we can't return reference
+// to a value owned by the function itself.
 
 // Signature
 