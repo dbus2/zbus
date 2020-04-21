d33ffb14161ffa03b7c53e6f036fd1cd667199c8
Marc-André Lureau <marcandre.lureau@redhat.com>
2020-04-22 01:13:33 +0200

    zb: add OwnedFd
    The FDs received over CMSG messages shouldn't be leaked in case of
    error. Let's have a helper type that closes automatically on drop.
    
    Note: I am having some thoughts for handling ownership in a safer way
    for dbus case, but for now this works well.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

---
+++ zbus/src/owned_fd.rs
@@ -0,0 +1,26 @@
+use std::mem::forget;
+use std::os::unix::io::{FromRawFd, IntoRawFd, RawFd};
+
+pub(crate) struct OwnedFd {
+    inner: RawFd,
+}
+
+impl FromRawFd for OwnedFd {
+    unsafe fn from_raw_fd(fd: RawFd) -> OwnedFd {
+        OwnedFd { inner: fd }
+    }
+}
+
+impl IntoRawFd for OwnedFd {
+    fn into_raw_fd(self) -> RawFd {
+        let v = self.inner;
+        forget(self);
+        v
+    }
+}
+
+impl Drop for OwnedFd {
+    fn drop(&mut self) {
+        let _ = nix::unistd::close(self.inner);
+    }
+}
