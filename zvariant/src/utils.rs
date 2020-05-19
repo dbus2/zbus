9921ef08e69ce4fc7f941e0775f45eb19ea464bf
Zeeshan Ali <zeeshanak@gnome.org>
2020-05-19 22:04:21 +0200

    zv: Variant signature constants now crate-public
    Users can get the signature string through Value::signature().

--- zvariant/src/utils.rs
+++ zvariant/src/utils.rs
@@ -13,8 +13,8 @@ pub const DICT_ENTRY_SIG_END_CHAR: char = '}';
 pub const DICT_ENTRY_SIG_START_STR: &str = "{";
 pub const DICT_ENTRY_SIG_END_STR: &str = "}";
 pub(crate) const DICT_ENTRY_ALIGNMENT: usize = 8;
-pub const VARIANT_SIGNATURE_CHAR: char = 'v';
-pub const VARIANT_SIGNATURE_STR: &str = "v";
+pub(crate) const VARIANT_SIGNATURE_CHAR: char = 'v';
+pub(crate) const VARIANT_SIGNATURE_STR: &str = "v";
 pub(crate) const VARIANT_ALIGNMENT: usize = 1;
 
 pub(crate) fn padding_for_n_bytes(value: usize, align: usize) -> usize {
