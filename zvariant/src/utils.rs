5a843449ced1fe7e54e275d87f099fd87a7335e3
Zeeshan Ali <zeeshanak@gnome.org>
2020-05-19 22:05:33 +0200

    zv: Document signature constants

--- zvariant/src/utils.rs
+++ zvariant/src/utils.rs
@@ -1,16 +1,26 @@
 use crate::{Basic, EncodingFormat, Error, Fd, ObjectPath, Signature};
 
+/// The prefix of ARRAY type signature, as a character. Provided for manual signature creation.
 pub const ARRAY_SIGNATURE_CHAR: char = 'a';
+/// The prefix of ARRAY type signature, as a string. Provided for manual signature creation.
 pub const ARRAY_SIGNATURE_STR: &str = "a";
 pub(crate) const ARRAY_ALIGNMENT: usize = 4;
+/// The opening character of STRUCT type signature. Provided for manual signature creation.
 pub const STRUCT_SIG_START_CHAR: char = '(';
+/// The closing character of STRUCT type signature. Provided for manual signature creation.
 pub const STRUCT_SIG_END_CHAR: char = ')';
+/// The opening character of STRUCT type signature, as a string. Provided for manual signature creation.
 pub const STRUCT_SIG_START_STR: &str = "(";
+/// The closing character of STRUCT type signature, as a string. Provided for manual signature creation.
 pub const STRUCT_SIG_END_STR: &str = ")";
 pub(crate) const STRUCT_ALIGNMENT: usize = 8;
+/// The opening character of DICT_ENTRY type signature. Provided for manual signature creation.
 pub const DICT_ENTRY_SIG_START_CHAR: char = '{';
+/// The closing character of DICT_ENTRY type signature. Provided for manual signature creation.
 pub const DICT_ENTRY_SIG_END_CHAR: char = '}';
+/// The opening character of DICT_ENTRY type signature, as a string. Provided for manual signature creation.
 pub const DICT_ENTRY_SIG_START_STR: &str = "{";
+/// The closing character of DICT_ENTRY type signature, as a string. Provided for manual signature creation.
 pub const DICT_ENTRY_SIG_END_STR: &str = "}";
 pub(crate) const DICT_ENTRY_ALIGNMENT: usize = 8;
 pub(crate) const VARIANT_SIGNATURE_CHAR: char = 'v';
