dc051e0832937c6c16ff4e10990c48003d08355b
Zeeshan Ali <zeeshanak@gnome.org>
2020-05-28 17:10:27 +0200

    zv: ObjectPath now byte slice based, rather than string
    Just like Signature, ObjectPath can only be ASCII characters and hence
    `[u8]` is a better representation anyway and it also improves performance
    I believe (untested but given that it does for Signature, it's a fairly
    safe bet I'd say).

--- zvariant/src/object_path.rs
+++ zvariant/src/object_path.rs
@@ -1,7 +1,7 @@
 use core::convert::TryFrom;
 use core::str;
 use serde::de::{Deserialize, Deserializer, Visitor};
-use serde::Serialize;
+use serde::ser::{Serialize, Serializer};
 use std::borrow::Cow;
 
 use crate::{Basic, Error, Result, Signature, Type};
@@ -31,33 +31,53 @@ use crate::{Basic, Error, Result, Signature, Type};
 /// ObjectPath::try_from("/end/with/slash/").unwrap_err();
 /// ObjectPath::try_from("/ha.d").unwrap_err();
 /// ```
-#[derive(Debug, PartialEq, Eq, Hash, Serialize, Clone)]
-#[serde(rename(
-    serialize = "zvariant::ObjectPath",
-    deserialize = "zvariant::ObjectPath"
-))]
-pub struct ObjectPath<'a>(#[serde(borrow)] Cow<'a, str>);
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub struct ObjectPath<'a>(Cow<'a, [u8]>);
 
 impl<'a> ObjectPath<'a> {
     /// The object path as a string.
     pub fn as_str(&self) -> &str {
+        // SAFETY: non-UTF8 characters in ObjectPath should NEVER happen
+        unsafe { str::from_utf8_unchecked(&self.0) }
+    }
+
+    /// The object path as bytes.
+    pub fn as_bytes(&self) -> &[u8] {
         &self.0
+    }
+
+    /// Create a new `ObjectPath` from given bytes.
+    ///
+    /// Since the passed bytes are not checked for correctness, prefer using the
+    /// `TryFrom<&[u8]>` implementation.
+    pub fn from_bytes_unchecked<'s: 'a>(bytes: &'s [u8]) -> Self {
+        Self(Cow::from(bytes))
     }
 
     /// Create a new `ObjectPath` from the given string.
     ///
     /// Since the passed string is not checked for correctness, prefer using the
     /// `TryFrom<&str>` implementation.
-    pub fn from_str_unchecked<'s: 'a>(signature: &'s str) -> Self {
-        Self(Cow::from(signature))
+    pub fn from_str_unchecked<'s: 'a>(path: &'s str) -> Self {
+        Self::from_bytes_unchecked(path.as_bytes())
     }
 
     /// Same as `from_str_unchecked`, except it takes an owned `String`.
     ///
     /// Since the passed string is not checked for correctness, prefer using the
     /// `TryFrom<String>` implementation.
-    pub fn from_string_unchecked(signature: String) -> Self {
-        Self(Cow::from(signature))
+    pub fn from_string_unchecked(path: String) -> Self {
+        Self(Cow::from(path.as_bytes().to_owned()))
+    }
+
+    /// the object path's length.
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    /// if the object path is empty.
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
     }
 
     pub(crate) fn to_owned(&self) -> ObjectPath<'static> {
@@ -78,14 +98,22 @@ impl<'a> Type for ObjectPath<'a> {
     }
 }
 
+impl<'a> TryFrom<&'a [u8]> for ObjectPath<'a> {
+    type Error = Error;
+
+    fn try_from(value: &'a [u8]) -> Result<Self> {
+        ensure_correct_object_path_str(value)?;
+
+        Ok(Self::from_bytes_unchecked(value))
+    }
+}
+
 /// Try to create an ObjectPath from a string.
 impl<'a> TryFrom<&'a str> for ObjectPath<'a> {
     type Error = Error;
 
     fn try_from(value: &'a str) -> Result<Self> {
-        ensure_correct_object_path_str(value)?;
-
-        Ok(Self(Cow::from(value)))
+        Self::try_from(value.as_bytes())
     }
 }
 
@@ -93,9 +121,9 @@ impl<'a> TryFrom<String> for ObjectPath<'a> {
     type Error = Error;
 
     fn try_from(value: String) -> Result<Self> {
-        ensure_correct_object_path_str(&value)?;
+        ensure_correct_object_path_str(value.as_bytes())?;
 
-        Ok(Self(Cow::from(value)))
+        Ok(Self::from_string_unchecked(value))
     }
 }
 
@@ -115,7 +143,16 @@ impl<'a> PartialEq<&str> for ObjectPath<'a> {
 
 impl<'a> std::fmt::Display for ObjectPath<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.0.fmt(f)
+        self.as_str().fmt(f)
+    }
+}
+
+impl<'a> Serialize for ObjectPath<'a> {
+    fn serialize<S>(&self, serializer: S) -> core::result::Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(self.as_str())
     }
 }
 
@@ -156,8 +193,8 @@ impl<'de> Visitor<'de> for ObjectPathVisitor {
     }
 }
 
-fn ensure_correct_object_path_str(path: &str) -> Result<()> {
-    let mut prev = '\0';
+fn ensure_correct_object_path_str(path: &[u8]) -> Result<()> {
+    let mut prev = b'\0';
 
     // Rules
     //
@@ -170,25 +207,27 @@ fn ensure_correct_object_path_str(path: &str) -> Result<()> {
         return Err(serde::de::Error::invalid_length(0, &"> 0 character"));
     }
 
-    for (i, c) in path.chars().enumerate() {
-        if i == 0 && c != '/' {
+    for i in 0..path.len() {
+        let c = path[i];
+
+        if i == 0 && c != b'/' {
             return Err(serde::de::Error::invalid_value(
-                serde::de::Unexpected::Char(c),
+                serde::de::Unexpected::Char(c as char),
                 &"/",
             ));
-        } else if c == '/' && prev == '/' {
+        } else if c == b'/' && prev == b'/' {
             return Err(serde::de::Error::invalid_value(
                 serde::de::Unexpected::Str("//"),
                 &"/",
             ));
-        } else if path.len() > 1 && i == (path.len() - 1) && c == '/' {
+        } else if path.len() > 1 && i == (path.len() - 1) && c == b'/' {
             return Err(serde::de::Error::invalid_value(
                 serde::de::Unexpected::Char('/'),
                 &"an alphanumeric character or `_`",
             ));
-        } else if !c.is_ascii_alphanumeric() && c != '/' && c != '_' {
+        } else if !c.is_ascii_alphanumeric() && c != b'/' && c != b'_' {
             return Err(serde::de::Error::invalid_value(
-                serde::de::Unexpected::Char(c),
+                serde::de::Unexpected::Char(c as char),
                 &"an alphanumeric character, `_` or `/`",
             ));
         }
