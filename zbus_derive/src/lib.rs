0a48a5456808588c9927764d6c4246c2a24422b0
Marc-André Lureau <marcandre.lureau@redhat.com>
2020-06-01 15:50:36 +0200

    zb-derive: learn to override guessed name
    This is useful when PascalCase<->snake_case case conversion isn't
    idempotent, or when more friendly/rusty names are preferred.

--- zbus_derive/src/lib.rs
+++ zbus_derive/src/lib.rs
@@ -110,10 +110,26 @@ pub fn dbus_proxy(attr: TokenStream, mut item: TokenStream) -> TokenStream {
                 }
 
                 let attrs = parse_item_attributes(&m.attrs).unwrap();
-                let m = if attrs.iter().any(|x| x.is_property()) {
-                    gen_proxy_property(&method_name, &m)
+                let is_property = attrs.iter().any(|x| x.is_property());
+                let has_inputs = m.sig.inputs.len() > 1;
+                let name = attrs
+                    .iter()
+                    .find_map(|x| match x {
+                        ItemAttribute::Name(n) => Some(n.to_string()),
+                        _ => None,
+                    })
+                    .unwrap_or_else(|| {
+                        pascal_case(if is_property && has_inputs {
+                            assert!(method_name.starts_with("set_"));
+                            &method_name[4..]
+                        } else {
+                            &method_name
+                        })
+                    });
+                let m = if is_property {
+                    gen_proxy_property(&name, &m)
                 } else {
-                    gen_proxy_method_call(&method_name, &m)
+                    gen_proxy_method_call(&name, &m)
                 };
                 methods.extend(m);
             }
@@ -169,7 +185,6 @@ fn gen_proxy_method_call(method_name: &str, m: &TraitItemMethod) -> proc_macro2:
         .iter()
         .filter_map(|arg| arg_ident(arg))
         .collect::<Vec<_>>();
-    let method_name = pascal_case(method_name);
     let sig = &m.sig;
     quote! {
         #(#doc)*
@@ -184,8 +199,6 @@ fn gen_proxy_property(property_name: &str, m: &TraitItemMethod) -> proc_macro2::
     let doc = get_doc_attrs(&m.attrs);
     let sig = &m.sig;
     if sig.inputs.len() > 1 {
-        assert!(property_name.starts_with("set_"));
-        let property_name = pascal_case(&property_name[4..]);
         let value = arg_ident(sig.inputs.last().unwrap()).unwrap();
         quote! {
             #(#doc)*
@@ -194,7 +207,6 @@ fn gen_proxy_property(property_name: &str, m: &TraitItemMethod) -> proc_macro2::
             }
         }
     } else {
-        let property_name = pascal_case(property_name);
         quote! {
             #(#doc)*
             pub #sig {
