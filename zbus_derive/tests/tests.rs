58ba5ea5543916e6fead48c989dbfe97f6b4593d
Marc-André Lureau <marcandre.lureau@redhat.com>
2020-06-05 00:41:13 +0200

    zb-derive: add simple proxy macro test
    (it would be nice to have some kind of compile-time assert or
    reflections, I don't know how yet)

---
+++ zbus_derive/tests/tests.rs
@@ -0,0 +1,24 @@
+use zbus;
+use zbus_derive::dbus_proxy;
+
+#[test]
+fn test_proxy() {
+    #[dbus_proxy(
+        interface = "org.freedesktop.zbus.Test",
+        default_service = "org.freedesktop.zbus",
+        default_path = "/org/freedesktop/zbus/test"
+    )]
+    trait Test {
+        /// comment for a_test()
+        fn a_test(&self, val: &str) -> zbus::Result<u32>;
+
+        #[dbus_proxy(name = "CheckRENAMING")]
+        fn check_renaming(&self) -> zbus::Result<Vec<u8>>;
+
+        #[dbus_proxy(property)]
+        fn property(&self) -> zbus::Result<Vec<String>>;
+
+        #[dbus_proxy(property)]
+        fn set_property(&self, val: u16) -> zbus::Result<()>;
+    }
+}
