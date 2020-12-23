a71f31d70a4047445cd1352fe10eaa82246b75be
Zeeshan Ali <zeeshanak@gnome.org>
2020-12-23 14:58:26 +0100

    zb: Add high-level API for handling signals
    However, this still won't be most convenient to use as user will
    typically need to wait for signals on a bunch of proxies at the same
    time. We'll need a way to group proxies so users can wait for signals on
    them.

--- zbus/src/proxy.rs
+++ zbus/src/proxy.rs
@@ -1,12 +1,18 @@
 use std::{
     borrow::Cow,
+    collections::HashMap,
     convert::{TryFrom, TryInto},
+    sync::Mutex,
 };
-use zvariant::{OwnedValue, Value};
+use zvariant::{ObjectPath, OwnedValue, Value};
 
 use crate::{Connection, Error, Message, Result};
 
 use crate::fdo::{self, IntrospectableProxy, PropertiesProxy};
+
+const LOCK_FAIL_MSG: &str = "Failed to lock a mutex or read-write lock";
+
+type SignalHandler = Box<dyn FnMut(&Message) -> Result<()> + Send>;
 
 /// A client-side interface proxy.
 ///
@@ -42,11 +48,11 @@ use crate::fdo::{self, IntrospectableProxy, PropertiesProxy};
 ///
 /// ## Current limitations:
 ///
-/// At this point, the `Proxy` is "simple". Notably, it doesn't:
-/// - have any signal handling
-/// - cache properties
-/// - track the current name owner
-/// - prevent auto-launching
+/// At the moment, `Proxy` doesn't:
+///
+/// * cache properties
+/// * track the current name owner
+/// * prevent auto-launching
 ///
 /// [`dbus_proxy`]: attr.dbus_proxy.html
 pub struct Proxy<'a> {
@@ -54,6 +60,7 @@ pub struct Proxy<'a> {
     destination: Cow<'a, str>,
     path: Cow<'a, str>,
     interface: Cow<'a, str>,
+    sig_handlers: Mutex<HashMap<&'static str, SignalHandler>>,
 }
 
 impl<'a> Proxy<'a> {
@@ -69,6 +76,7 @@ impl<'a> Proxy<'a> {
             destination: Cow::from(destination),
             path: Cow::from(path),
             interface: Cow::from(interface),
+            sig_handlers: Mutex::new(HashMap::new()),
         })
     }
 
@@ -85,6 +93,7 @@ impl<'a> Proxy<'a> {
             destination: Cow::from(destination),
             path: Cow::from(path),
             interface: Cow::from(interface),
+            sig_handlers: Mutex::new(HashMap::new()),
         })
     }
 
@@ -161,5 +170,204 @@ impl<'a> Proxy<'a> {
         R: serde::de::DeserializeOwned + zvariant::Type,
     {
         Ok(self.call_method(method_name, body)?.body()?)
+    }
+
+    /// Register a handler for signal named `signal_name`.
+    ///
+    /// Once a handler is successfully registered, call [`Self::next_signal`] to wait for the next
+    /// signal to arrive and be handled by its registered handler.
+    ///
+    /// If the associated connnection is to a bus, a match rule is added for the signal on the bus
+    /// so that the bus sends us the signals.
+    ///
+    /// ### Errors
+    ///
+    /// This method can fail if addition of the relevant match rule on the bus fails. You can
+    /// safely `unwrap` the `Result` if you're certain that associated connnection is not a bus
+    /// connection.
+    pub fn connect_signal<H>(&self, signal_name: &'static str, handler: H) -> fdo::Result<()>
+    where
+        H: FnMut(&Message) -> Result<()> + Send + 'static,
+    {
+        if self
+            .sig_handlers
+            .lock()
+            .expect(LOCK_FAIL_MSG)
+            .insert(signal_name, Box::new(handler))
+            .is_none()
+            && self.conn.is_bus()
+        {
+            let rule = self.match_rule_for_signal(signal_name);
+            fdo::DBusProxy::new(&self.conn)?.add_match(&rule)?;
+        }
+
+        Ok(())
+    }
+
+    /// Deregister the handler for the signal named `signal_name`.
+    ///
+    /// If the associated connnection is to a bus, the match rule is removed for the signal on the
+    /// bus so that the bus stops sending us the signal. This method returns `Ok(true)` if a
+    /// handler was registered for `signal_name` and was removed by this call; `Ok(false)`
+    /// otherwise.
+    ///
+    /// ### Errors
+    ///
+    /// This method can fail if removal of the relevant match rule on the bus fails. You can
+    /// safely `unwrap` the `Result` if you're certain that associated connnection is not a bus
+    /// connection.
+    pub fn disconnect_signal(&self, signal_name: &'static str) -> fdo::Result<bool> {
+        if self
+            .sig_handlers
+            .lock()
+            .expect(LOCK_FAIL_MSG)
+            .remove(signal_name)
+            .is_some()
+            && self.conn.is_bus()
+        {
+            let rule = self.match_rule_for_signal(signal_name);
+            fdo::DBusProxy::new(&self.conn)?.remove_match(&rule)?;
+
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+
+    /// Receive and handle the next incoming signal on the associated connection.
+    ///
+    /// This method will wait for signal messages on the associated connection and call any
+    /// handlers registered through the [`Self::connect_signal`] method. Signal handlers can be
+    /// registered and deregistered from another threads during the call to this method.
+    ///
+    /// If the signal message was handled by a handler, `Ok(None)` is returned. Otherwise, the
+    /// received message is returned.
+    pub fn next_signal(&self) -> Result<Option<Message>> {
+        let msg = self.conn.receive_specific(|msg| {
+            let handlers = self.sig_handlers.lock().expect(LOCK_FAIL_MSG);
+            if handlers.is_empty() {
+                // No signal handers associated anymore so no need to continue.
+                return Ok(true);
+            }
+
+            let hdr = msg.header()?;
+
+            let member = match hdr.member()? {
+                Some(m) => m,
+                None => return Ok(false),
+            };
+
+            Ok(hdr.interface()? == Some(&self.interface)
+                && hdr.path()? == Some(&ObjectPath::try_from(self.path.as_ref())?)
+                && hdr.message_type()? == crate::MessageType::Signal
+                && handlers.contains_key(member))
+        })?;
+
+        if self.handle_signal(&msg)? {
+            Ok(None)
+        } else {
+            Ok(Some(msg))
+        }
+    }
+
+    /// Handle the provided signal message.
+    ///
+    /// Call any handlers registered through the [`Self::connect_signal`] method for the provided
+    /// signal message.
+    ///
+    /// If no errors are encountered, `Ok(true)` is returned if a handler was found and called for,
+    /// the signal; `Ok(false)` otherwise.
+    pub fn handle_signal(&self, msg: &Message) -> Result<bool> {
+        let mut handlers = self.sig_handlers.lock().expect(LOCK_FAIL_MSG);
+        if handlers.is_empty() {
+            return Ok(false);
+        }
+
+        let hdr = msg.header()?;
+        if let Some(name) = hdr.member()? {
+            if let Some(handler) = handlers.get_mut(name) {
+                handler(&msg)?;
+
+                return Ok(true);
+            }
+        }
+
+        Ok(false)
+    }
+
+    pub(crate) fn has_signal_handler(&self, signal_name: &str) -> bool {
+        self.sig_handlers
+            .lock()
+            .expect(LOCK_FAIL_MSG)
+            .contains_key(signal_name)
+    }
+
+    fn match_rule_for_signal(&self, signal_name: &'static str) -> String {
+        // FIXME: Use the API to create this once we've it (issue#69).
+        format!(
+            "type='signal',path_namespace='{}',interface='{}',member='{}'",
+            self.path, self.interface, signal_name,
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::Arc;
+
+    #[test]
+    fn signal() {
+        // Register a well-known name with the session bus and ensure we get the appropriate
+        // signals called for that.
+        let conn = Connection::new_session().unwrap();
+        let owner_change_signaled = Arc::new(Mutex::new(false));
+        let name_acquired_signaled = Arc::new(Mutex::new(false));
+
+        let proxy = Proxy::new(
+            &conn,
+            "org.freedesktop.DBus",
+            "/org/freedesktop/DBus",
+            "org.freedesktop.DBus",
+        )
+        .unwrap();
+
+        let well_known = "org.freedesktop.zbus.ProxySignalTest";
+        let unique_name = conn.unique_name().unwrap().to_string();
+        {
+            let well_known = well_known.clone();
+            let signaled = owner_change_signaled.clone();
+            proxy
+                .connect_signal("NameOwnerChanged", move |m| {
+                    let (name, _, new_owner) = m.body::<(&str, &str, &str)>()?;
+                    assert_eq!(name, well_known);
+                    assert_eq!(new_owner, unique_name);
+                    *signaled.lock().unwrap() = true;
+
+                    Ok(())
+                })
+                .unwrap();
+        }
+        {
+            let signaled = name_acquired_signaled.clone();
+            proxy
+                .connect_signal("NameAcquired", move |m| {
+                    assert_eq!(m.body::<&str>()?, well_known);
+                    *signaled.lock().unwrap() = true;
+
+                    Ok(())
+                })
+                .unwrap();
+        }
+
+        fdo::DBusProxy::new(&conn)
+            .unwrap()
+            .request_name(&well_known, fdo::RequestNameFlags::ReplaceExisting.into())
+            .unwrap();
+
+        proxy.next_signal().unwrap();
+        proxy.next_signal().unwrap();
+        assert!(*owner_change_signaled.lock().unwrap());
+        assert!(*name_acquired_signaled.lock().unwrap());
     }
 }
