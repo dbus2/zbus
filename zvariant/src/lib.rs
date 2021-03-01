29488f7481e3e6eaec569f244686eb51c9a610d3
Marc-André Lureau <marcandre.lureau@redhat.com>
2021-03-02 01:24:26 +0400

    zv: add tests for borrowed bytes and serde_bytes
    Without the byte array commits just before this commit, the added derive
    test fails, for some reason with:
    
    panicked at 'called `Result::unwrap()` on an `Err` value:
    Message("invalid type: sequence, expected a borrowed byte array")',
    zvariant/src/lib.rs:1413:62
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- zvariant/src/lib.rs
+++ zvariant/src/lib.rs
@@ -1313,6 +1313,23 @@ mod tests {
         assert_eq!(encoded.len(), 4);
         let decoded: NoReprEnum = from_slice(&encoded, ctxt).unwrap();
         assert_eq!(decoded, NoReprEnum::Variant2);
+
+        #[derive(Deserialize, Serialize, Type, PartialEq, Debug)]
+        struct AStruct<'s> {
+            field1: u16,
+            field2: &'s [u8],
+            field3: i64,
+        }
+        assert_eq!(AStruct::signature(), "(qayx)");
+        let s = AStruct {
+            field1: 0xFF_FF,
+            field2: &[77u8; 8],
+            field3: 0xFF_FF_FF_FF_FF_FF,
+        };
+        let encoded = to_bytes(ctxt, &s).unwrap();
+        assert_eq!(encoded.len(), 24);
+        let decoded: AStruct<'_> = from_slice(&encoded, ctxt).unwrap();
+        assert_eq!(decoded, s);
     }
 
     #[test]
@@ -1331,6 +1348,70 @@ mod tests {
         let v = vec![1, 2];
         let l = crate::serialized_size(ctxt, &('a', "abc", &v)).unwrap();
         assert_eq!(l, 28);
+    }
+
+    #[test]
+    #[cfg(feature = "serde_bytes")]
+    fn serde_bytes() {
+        use serde::{Deserialize, Serialize};
+        use serde_bytes::*;
+
+        let ctxt = Context::<LE>::new_dbus(0);
+        let ay = Bytes::new(&[77u8; 1_000_000]);
+        let encoded = to_bytes(ctxt, &ay).unwrap();
+        assert_eq!(encoded.len(), 1_000_004);
+        let decoded: ByteBuf = from_slice(&encoded, ctxt).unwrap();
+        assert_eq!(decoded.len(), 1_000_000);
+
+        #[derive(Deserialize, Serialize, Type, PartialEq, Debug)]
+        struct Struct<'s> {
+            field1: u16,
+            #[serde(with = "serde_bytes")]
+            field2: &'s [u8],
+            field3: i64,
+        }
+        assert_eq!(Struct::signature(), "(qayx)");
+        let s = Struct {
+            field1: 0xFF_FF,
+            field2: &[77u8; 512],
+            field3: 0xFF_FF_FF_FF_FF_FF,
+        };
+        let encoded = to_bytes(ctxt, &s).unwrap();
+        assert_eq!(encoded.len(), 528);
+        let decoded: Struct<'_> = from_slice(&encoded, ctxt).unwrap();
+        assert_eq!(decoded, s);
+    }
+
+    #[test]
+    #[cfg(all(feature = "serde_bytes", feature = "gvariant"))]
+    fn serde_bytes_gvariant() {
+        use serde::{Deserialize, Serialize};
+        use serde_bytes::*;
+
+        let ctxt = Context::<LE>::new_gvariant(0);
+        let ay = Bytes::new(&[77u8; 1_000_000]);
+        let encoded = to_bytes(ctxt, &ay).unwrap();
+        assert_eq!(encoded.len(), 1_000_000);
+        let decoded: ByteBuf = from_slice(&encoded, ctxt).unwrap();
+        assert_eq!(decoded.len(), 1_000_000);
+
+        #[derive(Deserialize, Serialize, Type, PartialEq, Debug)]
+        struct Struct<'s> {
+            field1: u16,
+            #[serde(with = "serde_bytes")]
+            field2: &'s [u8],
+            field3: i64,
+        }
+        assert_eq!(Struct::signature(), "(qayx)");
+        let s = Struct {
+            field1: 0xFF_FF,
+            field2: &[77u8; 512],
+            field3: 0xFF_FF_FF_FF_FF_FF,
+        };
+        let encoded = to_bytes(ctxt, &s).unwrap();
+        assert_eq!(encoded.len(), 530);
+        let decoded: Struct<'_> = from_slice(&encoded, ctxt).unwrap();
+        assert_eq!(decoded, s);
     }
 
     #[test]
