df236a6a3ea14695d0f1c69924794c79f09403bb
Zeeshan Ali <zeeshanak@gnome.org>
2021-06-04 18:41:19 +0200

    Move README sample doctest run from zbus to zbus_polkit
    So that we can drop the dev depenency of `zbus_polkit` from `zbus`,
    which sometimes cause interesting issues.

--- zbus/src/lib.rs
+++ zbus/src/lib.rs
@@ -139,7 +139,6 @@
 
 #[cfg(doctest)]
 mod doctests {
-    doc_comment::doctest!("../../README.md");
     // Book markdown checks
     doc_comment::doctest!("../../book/src/client.md");
     doc_comment::doctest!("../../book/src/concepts.md");
