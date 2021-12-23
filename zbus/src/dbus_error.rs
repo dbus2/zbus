d3a2e7242b5c3ab35052cb6e0606ce792364838d
Zeeshan Ali <zeeshanak@gnome.org>
2021-12-23 18:17:06 +0100

    zb: Improve docs for DBusError trait

--- zbus/src/dbus_error.rs
+++ zbus/src/dbus_error.rs
@@ -1,6 +1,13 @@
 use crate::{Message, MessageHeader, Result};
 
-/// An error type suitable for a dbus reply method
+/// A trait that needs to be implemented by error types to be returned from D-Bus methods.
+///
+/// Typically, you'd use the [`crate::fdo::Error`] since that covers quite a lot of failures but
+/// occasionally you might find yourself needing to use a custom error type. You'll need to
+/// implement this trait for your error type. The easiest way to achieve that is to make use of the
+/// [`DBusError` macro][dm].
+///
+/// [dm]: macro.DBusError.html
 pub trait DBusError {
     /// Generate an error reply message for the given method call.
     fn create_reply(&self, msg: &MessageHeader<'_>) -> Result<Message>;
