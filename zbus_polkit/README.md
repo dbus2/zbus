a024d4424957375c7130b39eda764490e74689cb
Zeeshan Ali <zeeshanak@gnome.org>
2022-01-09 18:05:41 +0100

    Separate README for each subcrate
    Among other benefits, we no longer will need to duplicate the subcrate
    docs with the README as we can make use of include_str! magic to load
    crate-level docs from the README itself.

---
+++ zbus_polkit/README.md
@@ -0,0 +1,36 @@
+<img src="https://gitlab.freedesktop.org/dbus/zbus/-/raw/main/zbus-pixels.gif" alt="zbus illustration" style="width: 100%;">
+
+# zbus_polkit
+
+[![](https://docs.rs/zbus_polkit/badge.svg)](https://docs.rs/zbus_polkit/) [![](https://img.shields.io/crates/v/zbus_polkit)](https://crates.io/crates/zbus_polkit)
+
+A crate to interact with [PolicyKit], a toolkit for defining and handling authorizations. It is used
+for allowing unprivileged processes to speak to privileged processes.
+
+**Status:** Stable.
+
+#### Example code
+
+```rust,no_run
+use zbus::Connection;
+use zbus_polkit::policykit1::*;
+
+// Although we use `async-std` here, you can use any async runtime of choice.
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let connection = Connection::system().await?;
+    let proxy = AuthorityProxy::new(&connection).await?;
+    let subject = Subject::new_for_owner(std::process::id(), None, None)?;
+    let result = proxy.check_authorization(
+        &subject,
+        "org.zbus.BeAwesome",
+        &std::collections::HashMap::new(),
+        CheckAuthorizationFlags::AllowUserInteraction.into(),
+        "",
+    ).await?;
+
+    Ok(())
+}
+```
+
+[PolicyKit]: https://gitlab.freedesktop.org/polkit/polkit/
--- zbus_polkit/src/lib.rs
+++ zbus_polkit/src/lib.rs
@@ -2,37 +2,7 @@
 #![doc(
     html_logo_url = "https://storage.googleapis.com/fdo-gitlab-uploads/project/avatar/3213/zbus-logomark.png"
 )]
-
-//! A crate to interact with PolicyKit.
-//!
-//! [PolicyKit] is a toolkit for defining and handling authorizations. It is used for allowing
-//! unprivileged processes to speak to privileged processes. As you can guess from the name, it is
-//! based on [zbus].
-//!
-//! ```no_run
-//! use zbus::Connection;
-//! use zbus_polkit::policykit1::*;
-//!
-//! // Although we use `async-std` here, you can use any async runtime of choice.
-//! #[async_std::main]
-//! async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//!     let connection = Connection::system().await?;
-//!     let proxy = AuthorityProxy::new(&connection).await?;
-//!     let subject = Subject::new_for_owner(std::process::id(), None, None)?;
-//!     let result = proxy.check_authorization(
-//!         &subject,
-//!         "org.zbus.BeAwesome",
-//!         &std::collections::HashMap::new(),
-//!         CheckAuthorizationFlags::AllowUserInteraction.into(),
-//!         "",
-//!     ).await?;
-//!
-//!     Ok(())
-//! }
-//! ```
-//!
-//! [PolicyKit]: https://gitlab.freedesktop.org/polkit/polkit/
-//! [zbus]: https://crates.io/crates/zbus
+#![doc = include_str!("../README.md")]
 
 mod error;
 pub use error::*;
@@ -41,5 +11,5 @@ pub mod policykit1;
 
 #[cfg(doctest)]
 mod doctests {
-    doc_comment::doctest!("../../README.md");
+    doc_comment::doctest!("../README.md");
 }
