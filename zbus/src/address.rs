031359ca4f32702de101001c266f3f678096a784
Kas <52051-exactly-one-kas@users.noreply.gitlab.freedesktop.org>
2022-06-02 17:43:03 +0200

    Add support for nonce-tcp addresses

--- zbus/src/address.rs
+++ zbus/src/address.rs
@@ -18,6 +18,8 @@ use tokio::net::UnixStream;
 use uds_windows::UnixStream;
 
 use std::ffi::OsString;
+use std::fmt::{Display, Formatter};
+use std::str::from_utf8_unchecked;
 
 /// A `tcp:` address family.
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
@@ -57,7 +59,7 @@ impl TcpAddress {
     }
 
     // Helper for FromStr
-    fn from_tcp(opts: HashMap<&str, &str>) -> Result<Address> {
+    fn from_tcp(opts: HashMap<&str, &str>) -> Result<Self> {
         let bind = None;
         if opts.contains_key("bind") {
             return Err(Error::Address("`bind` isn't yet supported".into()));
@@ -78,12 +80,31 @@ impl TcpAddress {
             .map(|f| TcpAddressFamily::from_str(f))
             .transpose()?;
 
-        Ok(Address::Tcp(TcpAddress {
+        Ok(Self {
             host,
             bind,
             port,
             family,
-        }))
+        })
+    }
+
+    fn write_options(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str("host=")?;
+
+        encode_percents(f, self.host.as_ref())?;
+
+        write!(f, ",port={}", self.port)?;
+
+        if let Some(bind) = &self.bind {
+            f.write_str(",bind=")?;
+            encode_percents(f, bind.as_ref())?;
+        }
+
+        if let Some(family) = &self.family {
+            write!(f, ",family={}", family)?;
+        }
+
+        Ok(())
     }
 }
 
@@ -95,6 +116,11 @@ pub enum Address {
     Unix(OsString),
     /// TCP address details
     Tcp(TcpAddress),
+    /// TCP address details with nonce file path
+    NonceTcp {
+        addr: TcpAddress,
+        nonce_file: Vec<u8>,
+    },
 }
 
 #[cfg(feature = "async-io")]
@@ -190,6 +216,41 @@ impl Address {
             }
 
             Address::Tcp(addr) => connect_tcp(addr).await.map(Stream::Tcp),
+
+            Address::NonceTcp { addr, nonce_file } => {
+                let mut stream = connect_tcp(addr).await?;
+
+                #[cfg(unix)]
+                let nonce_file = {
+                    use std::os::unix::ffi::OsStrExt;
+                    std::ffi::OsStr::from_bytes(&nonce_file)
+                };
+
+                #[cfg(windows)]
+                let nonce_file = std::str::from_utf8(&nonce_file)
+                    .map_err(|_| Error::Address("nonce file path is invalid UTF-8".to_owned()))?;
+
+                #[cfg(feature = "async-io")]
+                {
+                    let nonce = std::fs::read(nonce_file)?;
+                    let mut nonce = &nonce[..];
+
+                    while !nonce.is_empty() {
+                        let len = stream
+                            .write_with_mut(|s| std::io::Write::write(s, nonce))
+                            .await?;
+                        nonce = &nonce[len..];
+                    }
+                }
+
+                #[cfg(all(not(feature = "async-io"), feature = "tokio"))]
+                {
+                    let nonce = tokio::fs::read(nonce_file).await?;
+                    tokio::io::AsyncWriteExt::write_all(&mut stream, &nonce).await?;
+                }
+
+                Ok(Stream::Tcp(stream))
+            }
         }
     }
 
@@ -265,6 +326,125 @@ impl FromStr for TcpAddressFamily {
     }
 }
 
+impl Display for TcpAddressFamily {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Ipv4 => write!(f, "ipv4"),
+            Self::Ipv6 => write!(f, "ipv6"),
+        }
+    }
+}
+
+fn decode_hex(c: char) -> Result<u8> {
+    match c {
+        '0'..='9' => Ok(c as u8 - b'0'),
+        'a'..='f' => Ok(c as u8 - b'a' + 10),
+        'A'..='F' => Ok(c as u8 - b'A' + 10),
+
+        _ => Err(Error::Address(
+            "invalid hexadecimal character in percent-encoded sequence".to_owned(),
+        )),
+    }
+}
+
+fn decode_percents(value: &str) -> Result<Vec<u8>> {
+    let mut iter = value.chars();
+    let mut decoded = Vec::new();
+
+    while let Some(c) = iter.next() {
+        if matches!(c, '-' | 'A'..='Z' | 'a'..='z' | '_' | '/' | '.' | '\\' | '*') {
+            decoded.push(c as u8)
+        } else if c == '%' {
+            decoded.push(
+                decode_hex(iter.next().ok_or_else(|| {
+                    Error::Address("incomplete percent-encoded sequence".to_owned())
+                })?)?
+                    << 4
+                    | decode_hex(iter.next().ok_or_else(|| {
+                        Error::Address("incomplete percent-encoded sequence".to_owned())
+                    })?)?,
+            );
+        } else {
+            return Err(Error::Address("Invalid character in address".to_owned()));
+        }
+    }
+
+    Ok(decoded)
+}
+
+fn encode_percents(f: &mut Formatter<'_>, mut value: &[u8]) -> std::fmt::Result {
+    const LOOKUP: &str = "\
+%00%01%02%03%04%05%06%07%08%09%0a%0b%0c%0d%0e%0f\
+%10%11%12%13%14%15%16%17%18%19%1a%1b%1c%1d%1e%1f\
+%20%21%22%23%24%25%26%27%28%29%2a%2b%2c%2d%2e%2f\
+%30%31%32%33%34%35%36%37%38%39%3a%3b%3c%3d%3e%3f\
+%40%41%42%43%44%45%46%47%48%49%4a%4b%4c%4d%4e%4f\
+%50%51%52%53%54%55%56%57%58%59%5a%5b%5c%5d%5e%5f\
+%60%61%62%63%64%65%66%67%68%69%6a%6b%6c%6d%6e%6f\
+%70%71%72%73%74%75%76%77%78%79%7a%7b%7c%7d%7e%7f\
+%80%81%82%83%84%85%86%87%88%89%8a%8b%8c%8d%8e%8f\
+%90%91%92%93%94%95%96%97%98%99%9a%9b%9c%9d%9e%9f\
+%a0%a1%a2%a3%a4%a5%a6%a7%a8%a9%aa%ab%ac%ad%ae%af\
+%b0%b1%b2%b3%b4%b5%b6%b7%b8%b9%ba%bb%bc%bd%be%bf\
+%c0%c1%c2%c3%c4%c5%c6%c7%c8%c9%ca%cb%cc%cd%ce%cf\
+%d0%d1%d2%d3%d4%d5%d6%d7%d8%d9%da%db%dc%dd%de%df\
+%e0%e1%e2%e3%e4%e5%e6%e7%e8%e9%ea%eb%ec%ed%ee%ef\
+%f0%f1%f2%f3%f4%f5%f6%f7%f8%f9%fa%fb%fc%fd%fe%ff";
+
+    loop {
+        let pos = value.iter().position(
+            |c| !matches!(c, b'-' | b'A'..=b'Z' | b'a'..=b'z' | b'_' | b'/' | b'.' | b'\\' | b'*'),
+        );
+
+        if let Some(pos) = pos {
+            // SAFETY: The above `position()` call made sure that only ASCII chars are in the string up to `pos`
+            f.write_str(unsafe { from_utf8_unchecked(&value[..pos]) })?;
+
+            let c = value[pos];
+            value = &value[pos + 1..];
+
+            let pos = c as usize * 3;
+            f.write_str(&LOOKUP[pos..pos + 3])?;
+        } else {
+            // SAFETY: The above `position()` call made sure that only ASCII chars are in the rest of the string
+            f.write_str(unsafe { from_utf8_unchecked(value) })?;
+            return Ok(());
+        }
+    }
+}
+
+impl Display for Address {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Tcp(addr) => {
+                f.write_str("tcp:")?;
+                addr.write_options(f)?;
+            }
+
+            Self::NonceTcp { addr, nonce_file } => {
+                f.write_str("nonce-tcp:noncefile=")?;
+                encode_percents(f, nonce_file)?;
+                f.write_str(",")?;
+                addr.write_options(f)?;
+            }
+
+            Self::Unix(path) => {
+                #[cfg(unix)]
+                {
+                    use std::os::unix::ffi::OsStrExt;
+                    f.write_str("unix:path=")?;
+                    encode_percents(f, path.as_bytes())?;
+                }
+
+                #[cfg(windows)]
+                write!(f, "unix:path={}", path.to_str().ok_or(std::fmt::Error)?)?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
 impl FromStr for Address {
     type Err = Error;
 
@@ -272,13 +452,17 @@ impl FromStr for Address {
     fn from_str(address: &str) -> Result<Self> {
         let col = address
             .find(':')
-            .ok_or_else(|| Error::Address("address has no colon".into()))?;
+            .ok_or_else(|| Error::Address("address has no colon".to_owned()))?;
         let transport = &address[..col];
         let mut options = HashMap::new();
         for kv in address[col + 1..].split(',') {
             let (k, v) = match kv.find('=') {
                 Some(eq) => (&kv[..eq], &kv[eq + 1..]),
-                None => return Err(Error::Address("missing = when parsing key/value".into())),
+                None => {
+                    return Err(Error::Address(
+                        "missing = when parsing key/value".to_owned(),
+                    ))
+                }
             };
             if options.insert(k, v).is_some() {
                 return Err(Error::Address(format!(
@@ -289,9 +473,19 @@ impl FromStr for Address {
         }
 
         match transport {
-            #[cfg(unix)]
+            #[cfg(any(unix, feature = "async-io"))]
             "unix" => Self::from_unix(options),
-            "tcp" => Self::from_tcp(options),
+            "tcp" => TcpAddress::from_tcp(options).map(Self::Tcp),
+
+            "nonce-tcp" => Ok(Self::NonceTcp {
+                nonce_file: decode_percents(
+                    options
+                        .get("noncefile")
+                        .ok_or_else(|| Error::Address("missing nonce file parameter".into()))?,
+                )?,
+                addr: TcpAddress::from_tcp(options)?,
+            }),
+
             _ => Err(Error::Address(format!(
                 "unsupported transport '{}'",
                 transport
@@ -345,24 +539,20 @@ mod tests {
             Error::Address(e) => assert_eq!(e, "invalid tcp address `family`: ipv7"),
             _ => panic!(),
         }
-        #[cfg(unix)]
         match Address::from_str("unix:foo=blah").unwrap_err() {
             Error::Address(e) => assert_eq!(e, "unix address is missing path or abstract"),
             _ => panic!(),
         }
-        #[cfg(unix)]
         match Address::from_str("unix:path=/tmp,abstract=foo").unwrap_err() {
             Error::Address(e) => {
                 assert_eq!(e, "`path` and `abstract` cannot be specified together")
             }
             _ => panic!(),
         }
-        #[cfg(unix)]
         assert_eq!(
             Address::Unix("/tmp/dbus-foo".into()),
             Address::from_str("unix:path=/tmp/dbus-foo").unwrap()
         );
-        #[cfg(unix)]
         assert_eq!(
             Address::Unix("/tmp/dbus-foo".into()),
             Address::from_str("unix:path=/tmp/dbus-foo,guid=123").unwrap()
@@ -393,6 +583,82 @@ mod tests {
                 family: Some(TcpAddressFamily::Ipv6)
             }),
             Address::from_str("tcp:host=localhost,port=4142,family=ipv6").unwrap()
+        );
+        assert_eq!(
+            Address::Tcp(TcpAddress {
+                host: "localhost".into(),
+                port: 4142,
+                bind: None,
+                family: Some(TcpAddressFamily::Ipv6)
+            }),
+            Address::from_str("tcp:host=localhost,port=4142,family=ipv6,noncefile=/a/file/path")
+                .unwrap()
+        );
+        assert_eq!(
+            Address::NonceTcp {
+                addr: TcpAddress {
+                    host: "localhost".into(),
+                    port: 4142,
+                    bind: None,
+                    family: Some(TcpAddressFamily::Ipv6),
+                },
+                nonce_file: b"/a/file/path to file".to_vec()
+            },
+            Address::from_str(
+                "nonce-tcp:host=localhost,port=4142,family=ipv6,noncefile=/a/file/path%20to%20file"
+            )
+            .unwrap()
+        );
+    }
+
+    #[test]
+    fn stringify_dbus_addresses() {
+        assert_eq!(
+            Address::Unix("/tmp/dbus-foo".into()).to_string(),
+            "unix:path=/tmp/dbus-foo"
+        );
+        assert_eq!(
+            Address::Tcp(TcpAddress {
+                host: "localhost".into(),
+                port: 4142,
+                bind: None,
+                family: None
+            })
+            .to_string(),
+            "tcp:host=localhost,port=4142"
+        );
+        assert_eq!(
+            Address::Tcp(TcpAddress {
+                host: "localhost".into(),
+                port: 4142,
+                bind: None,
+                family: Some(TcpAddressFamily::Ipv4)
+            })
+            .to_string(),
+            "tcp:host=localhost,port=4142,family=ipv4"
+        );
+        assert_eq!(
+            Address::Tcp(TcpAddress {
+                host: "localhost".into(),
+                port: 4142,
+                bind: None,
+                family: Some(TcpAddressFamily::Ipv6)
+            })
+            .to_string(),
+            "tcp:host=localhost,port=4142,family=ipv6"
+        );
+        assert_eq!(
+            Address::NonceTcp {
+                addr: TcpAddress {
+                    host: "localhost".into(),
+                    port: 4142,
+                    bind: None,
+                    family: Some(TcpAddressFamily::Ipv6),
+                },
+                nonce_file: b"/a/file/path to file".to_vec()
+            }
+            .to_string(),
+            "nonce-tcp:noncefile=/a/file/path%20to%20file,host=localhost,port=4142,family=ipv6"
         );
     }
 
