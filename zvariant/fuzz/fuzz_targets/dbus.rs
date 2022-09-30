e31422160e2b7331aa19cbcfa61da786a371b2b6
Zeeshan Ali <zeeshanak@gnome.org>
2022-09-30 17:49:07 +0200

    zv: Fuzz w/ big endian as well

--- zvariant/fuzz/fuzz_targets/dbus.rs
+++ zvariant/fuzz/fuzz_targets/dbus.rs
@@ -3,4 +3,5 @@ mod utils;
 
 libfuzzer_sys::fuzz_target!(|data: &[u8]| {
     utils::fuzz_for_context(data, zvariant::EncodingContext::<byteorder::LE>::new_dbus(0));
+    utils::fuzz_for_context(data, zvariant::EncodingContext::<byteorder::BE>::new_dbus(0));
 });
--- zvariant/fuzz/fuzz_targets/gvariant.rs
+++ zvariant/fuzz/fuzz_targets/gvariant.rs
@@ -3,4 +3,5 @@ mod utils;
 
 libfuzzer_sys::fuzz_target!(|data: &[u8]| {
     utils::fuzz_for_context(data, zvariant::EncodingContext::<byteorder::LE>::new_gvariant(0));
+    utils::fuzz_for_context(data, zvariant::EncodingContext::<byteorder::BE>::new_gvariant(0));
 });
