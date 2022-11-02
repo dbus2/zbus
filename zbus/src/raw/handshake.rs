2f7411dae4338ebafcf0e53aa52e39404e0146bd
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2022-11-02 12:36:50 +0100

    Make check happy

--- zbus/src/raw/handshake.rs
+++ zbus/src/raw/handshake.rs
@@ -226,7 +226,7 @@ impl<S: Socket> ClientHandshake<S> {
                 let cookie = Cookie::lookup(name, id)?;
                 let client_chall = random_ascii(16);
                 let sec = format!("{}:{}:{}", server_chall, client_chall, cookie);
-                let sha1 = hex::encode(sha1::Sha1::digest(sec));
+                let sha1 = hex::encode(Sha1::digest(sec));
                 let data = format!("{} {}", client_chall, sha1);
                 Ok((WaitingForOK, Command::Data(data.into())))
             }
