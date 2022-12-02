dbus2/zbus

commit 17a04a4979d841760d0bda46f1ce46ec4e41e319

zb: Internal Connection::call_method_raw more consistent
Just like other call_method calls, this shouldn't require caller to
assemble the message but rather be generic.

zbus/src/connection.rs
@@ -33,7 +33,8 @@ use crate::{
     fdo::{self, RequestNameFlags, RequestNameReply},
     raw::{Connection as RawConnection, Socket},
     Authenticated, CacheProperties, ConnectionBuilder, DBusError, Error, Executor, Guid, Message,
-    MessageStream, MessageType, ObjectServer, OwnedMatchRule, Result, Task,
+    MessageBuilder, MessageFlags, MessageStream, MessageType, ObjectServer, OwnedMatchRule, Result,
+    Task,
 };
 
 const DEFAULT_MAX_QUEUED: usize = 64;
@@ -384,15 +385,17 @@ impl Connection {
         M::Error: Into<Error>,
         B: serde::ser::Serialize + zvariant::DynamicType,
     {
-        let m = Message::method(
-            self.unique_name(),
+        self.call_method_raw(
             destination,
             path,
             interface,
             method_name,
+            BitFlags::empty(),
             body,
-        )?;
-        self.call_method_raw(m).await?.await
+        )
+        .await?
+        .expect("no reply")
+        .await
     }
 
     /// Send a method call.
@@ -400,13 +403,50 @@ impl Connection {
     /// Send the given message, which must be a method call, over the connection and return an
     /// object that allows the reply to be retrieved.  Typically you'd want to use
     /// [`Connection::call_method`] instead.
-    pub(crate) async fn call_method_raw(&self, msg: Message) -> Result<PendingMethodCall> {
-        debug_assert_eq!(msg.message_type(), MessageType::MethodCall);
+    ///
+    /// If the `flags` do not contain `MethodFlags::NoReplyExpected`, the return value is
+    /// guaranteed to be `Ok(Some(_))`, if there was no error encountered.
+    ///
+    /// INTERNAL NOTE: If this method is ever made pub, flags should become `BitFlags<MethodFlags>`.
+    pub(crate) async fn call_method_raw<'d, 'p, 'i, 'm, D, P, I, M, B>(
+        &self,
+        destination: Option<D>,
+        path: P,
+        interface: Option<I>,
+        method_name: M,
+        flags: BitFlags<MessageFlags>,
+        body: &B,
+    ) -> Result<Option<PendingMethodCall>>
+    where
+        D: TryInto<BusName<'d>>,
+        P: TryInto<ObjectPath<'p>>,
+        I: TryInto<InterfaceName<'i>>,
+        M: TryInto<MemberName<'m>>,
+        D::Error: Into<Error>,
+        P::Error: Into<Error>,
+        I::Error: Into<Error>,
+        M::Error: Into<Error>,
+        B: serde::ser::Serialize + zvariant::DynamicType,
+    {
+        let mut builder = MessageBuilder::method_call(path, method_name)?;
+        if let Some(destination) = destination {
+            builder = builder.destination(destination)?
+        }
+        if let Some(interface) = interface {
+            builder = builder.interface(interface)?
+        }
+        for flag in flags {
+            builder = builder.with_flags(flag)?;
+        }
+        let msg = builder.build(body)?;
 
         let stream = Some(MessageStream::from(self.clone()));
         let serial = self.send_message(msg).await?;
-
-        Ok(PendingMethodCall { stream, serial })
+        if flags.contains(MessageFlags::NoReplyExpected) {
+            Ok(None)
+        } else {
+            Ok(Some(PendingMethodCall { stream, serial }))
+        }
     }
 
     /// Emit a signal.

zbus/src/proxy.rs
@@ -23,7 +23,7 @@ use zvariant::{ObjectPath, OwnedValue, Str, Value};
 use crate::{
     async_channel::channel,
     fdo::{self, IntrospectableProxy, NameOwnerChanged, PropertiesProxy},
-    CacheProperties, Connection, Error, Executor, MatchRule, Message, MessageBuilder,
+    CacheProperties, Connection, Error, Executor, MatchRule, Message, MessageBuilder, MessageFlags,
     MessageSequence, MessageType, OwnedMatchRule, ProxyBuilder, Result, Task,
 };
 
@@ -320,16 +320,18 @@ impl PropertiesCache {
             .await
             .map(|s| s.map(Either::Left))?;
 
-        let get_all = MessageBuilder::method_call(proxy.path().as_ref(), "GetAll")?
-            .destination(proxy.destination())?
-            .interface(proxy.interface())?
-            .build(&interface)?;
-
         let get_all = proxy
             .connection()
-            .call_method_raw(get_all)
+            .call_method_raw(
+                Some(proxy.destination()),
+                proxy.path(),
+                Some(proxy.interface()),
+                "GetAll",
+                BitFlags::empty(),
+                &interface,
+            )
             .await
-            .map(|s| FromFuture::from(s).map(Either::Right))?;
+            .map(|r| FromFuture::from(r.expect("no reply")).map(Either::Right))?;
 
         let mut join = join_streams(prop_changes, get_all);
 
@@ -819,35 +821,27 @@ impl<'a> Proxy<'a> {
         B: serde::ser::Serialize + zvariant::DynamicType,
         R: serde::de::DeserializeOwned + zvariant::Type,
     {
-        let mut builder = MessageBuilder::method_call(self.inner.path.as_ref(), method_name)?
-            .destination(&self.inner.destination)?
-            .interface(&self.inner.interface)?;
-
-        let msg_flags = flags.into();
-        for flag in msg_flags {
-            builder = builder.with_flags(flag.into())?;
-        }
-
-        let msg = builder.build(body)?;
-
-        if msg_flags.contains(MethodFlags::NoReplyExpected) {
-            self.inner
-                .inner_without_borrows
-                .conn
-                .send_message(msg)
-                .await?;
-            Ok(None)
-        } else {
-            let response = self
-                .inner
-                .inner_without_borrows
-                .conn
-                .call_method_raw(msg)
-                .await?
-                .await?
-                .body()?;
-
-            Ok(Some(response))
+        let flags = flags
+            .into()
+            .iter()
+            .map(MessageFlags::from)
+            .collect::<BitFlags<_>>();
+        match self
+            .inner
+            .inner_without_borrows
+            .conn
+            .call_method_raw(
+                Some(self.destination()),
+                self.path(),
+                Some(self.interface()),
+                method_name,
+                flags,
+                body,
+            )
+            .await?
+        {
+            Some(reply) => reply.await?.body().map(Some),
+            None => Ok(None),
         }
     }
 
@@ -1046,7 +1040,7 @@ pub enum MethodFlags {
 
 assert_impl_all!(MethodFlags: Send, Sync, Unpin);
 
-impl From<MethodFlags> for crate::MessageFlags {
+impl From<MethodFlags> for MessageFlags {
     fn from(method_flag: MethodFlags) -> Self {
         match method_flag {
             MethodFlags::NoReplyExpected => Self::NoReplyExpected,

commit 9942c46aa61d02cf25387a25743e7db40ebca1ec

zb: Set sender of method calls
This fixes a regression from 17a04a49.

zbus/src/connection.rs
@@ -429,6 +429,9 @@ impl Connection {
         B: serde::ser::Serialize + zvariant::DynamicType,
     {
         let mut builder = MessageBuilder::method_call(path, method_name)?;
+        if let Some(sender) = self.unique_name() {
+            builder = builder.sender(sender)?
+        }
         if let Some(destination) = destination {
             builder = builder.destination(destination)?
         }