dbus2/zbus

commit 2c010d57c0bd1255342e4ba807745ced11cbf250

zb: ObjectServer to only serve methods destined to its connection
While in most cases, you'd want ObjectServer to take over all incoming
method calls on its connection but in certain cases, you don't. For
example, if you'd build a bus implementation on top of zbus, you'll
likely want to make use of ObjectServer for the Freedesktop standard
interfaces required from a bus, while being able to receive messages on
the same connections destined for other peers to send them off to the
right connection. If ObjectServer would serve all messages, it'll just
return an error for all method calls destined for other peers.

I'm sure there are other (not so niche) use cases for this.

zbus/src/connection.rs
@@ -17,7 +17,7 @@ use std::{
     },
     task::{Context, Poll},
 };
-use tracing::{debug, instrument, trace, trace_span, Instrument};
+use tracing::{debug, instrument, trace, trace_span, warn, Instrument};
 use zbus_names::{BusName, ErrorName, InterfaceName, MemberName, OwnedUniqueName, WellKnownName};
 use zvariant::ObjectPath;
 
@@ -709,8 +709,47 @@ impl Connection {
                         }
                         m.ok()
                     }) {
-                        trace!("Got `{}`. Will spawn a task for dispatch..", msg);
                         if let Some(conn) = weak_conn.upgrade() {
+                            let hdr = match msg.header() {
+                                Ok(hdr) => hdr,
+                                Err(e) => {
+                                    warn!("Failed to parse header: {}", e);
+
+                                    continue;
+                                }
+                            };
+                            match hdr.destination() {
+                                Ok(Some(BusName::Unique(dest))) => {
+                                    match conn.unique_name().map(|n| &**n) {
+                                        Some(unique_name) if dest != unique_name => {
+                                            trace!("Got a method call for a different destination: {}", dest);
+
+                                            continue;
+                                        }
+                                        _ => (),
+                                    }
+                                }
+                                Ok(Some(BusName::WellKnown(dest))) => {
+                                    let names = conn.inner.registered_names.lock().await;
+                                    // For p2p, destination doesn't matter if no name has been registered.
+                                    if !names.contains(dest) && (conn.is_bus() || !names.is_empty()) {
+                                        trace!("Got a method call for a different destination: {}", dest);
+
+                                        continue;
+                                    }
+                                }
+                                Ok(None) => {
+                                    warn!("Got a method call with no destination: {}", msg);
+
+                                    continue;
+                                }
+                                Err(e) => {
+                                    warn!("Failed to parse destination: {}", e);
+
+                                    continue;
+                                }
+                            }
+                            trace!("Got `{}`. Will spawn a task for dispatch..", msg);
                             let executor = conn.inner.executor.clone();
                             executor
                                 .spawn(

commit 9cf47b57130747ab29f883d944620daab4d98393

zbus: the ObjectServer should dispatch calls without destination
p2p connections typically don't have destination.

Testing this with zbus proxy is a bit of a pain, since it enforces a
destination at all level. We should fix that too, but that will break API.

Fixes: commit 2c010d57c0 ("zb: ObjectServer to only serve methods destined to its connection")
Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

zbus/src/connection.rs
@@ -989,7 +989,7 @@ impl Connection {
                             };
                             match hdr.destination() {
                                 // Unique name is already checked by the match rule.
-                                Ok(Some(BusName::Unique(_))) => (),
+                                Ok(Some(BusName::Unique(_))) | Ok(None) => (),
                                 Ok(Some(BusName::WellKnown(dest))) => {
                                     let names = conn.inner.registered_names.lock().await;
                                     // destination doesn't matter if no name has been registered
@@ -999,11 +999,6 @@ impl Connection {
 
                                         continue;
                                     }
-                                }
-                                Ok(None) => {
-                                    warn!("Got a method call with no destination: {}", msg);
-
-                                    continue;
                                 }
                                 Err(e) => {
                                     warn!("Failed to parse destination: {}", e);