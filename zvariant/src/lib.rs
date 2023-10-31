84ac3e00a2b51d597e611eb9dd4d19534798ff82
Maximilian Güntner <code@mguentner.de>
2023-10-31 11:59:55 +0100

    🧪 zv: add dict_compare test
    this test is failing

--- zvariant/src/lib.rs
+++ zvariant/src/lib.rs
@@ -1257,6 +1257,21 @@ mod tests {
     }
 
     #[test]
+    fn dict_compare() {
+        // the order in which a dict has been constructed must not play a role
+        // https://github.com/dbus2/zbus/issues/484
+        let mut dict1 = Dict::new(<&str>::signature(), Value::signature());
+        dict1.add("first", Value::new("value")).unwrap();
+        dict1.add("second", Value::new("value")).unwrap();
+
+        let mut dict2 = Dict::new(<&str>::signature(), Value::signature());
+        dict2.add("second", Value::new("value")).unwrap();
+        dict2.add("first", Value::new("value")).unwrap();
+
+        assert_eq!(dict1, dict2);
+    }
+
+    #[test]
     fn value_value() {
         let ctxt = Context::<BE>::new_dbus(0);
         let encoded = to_bytes(ctxt, &0xABBA_ABBA_ABBA_ABBA_u64).unwrap();
