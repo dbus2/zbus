dbus2/zbus

commit d137afdeabeefb552f754cdba301444477b4757f

🚚 zb: Move vsock socket type impls to their own module

zbus/src/connection/socket/vsock.rs
@@ -1,0 +1,106 @@
+#[cfg(feature = "tokio-vsock")]
+use super::{Socket, Split};
+
+#[cfg(all(feature = "vsock", not(feature = "tokio")))]
+#[async_trait::async_trait]
+impl super::ReadHalf for std::sync::Arc<async_io::Async<vsock::VsockStream>> {
+    async fn recvmsg(&mut self, buf: &mut [u8]) -> super::RecvmsgResult {
+        match futures_util::AsyncReadExt::read(&mut self.as_ref(), buf).await {
+            Err(e) => Err(e),
+            Ok(len) => {
+                #[cfg(unix)]
+                let ret = (len, vec![]);
+                #[cfg(not(unix))]
+                let ret = len;
+                Ok(ret)
+            }
+        }
+    }
+}
+
+#[cfg(all(feature = "vsock", not(feature = "tokio")))]
+#[async_trait::async_trait]
+impl super::WriteHalf for std::sync::Arc<async_io::Async<vsock::VsockStream>> {
+    async fn sendmsg(
+        &mut self,
+        buf: &[u8],
+        #[cfg(unix)] fds: &[std::os::unix::io::RawFd],
+    ) -> std::io::Result<usize> {
+        use std::io;
+
+        #[cfg(unix)]
+        if !fds.is_empty() {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "fds cannot be sent with a vsock stream",
+            ));
+        }
+
+        futures_util::AsyncWriteExt::write(&mut self.as_ref(), buf).await
+    }
+
+    async fn close(&self) -> io::Result<()> {
+        let stream = self.clone();
+        crate::Task::spawn_blocking(
+            move || stream.get_ref().shutdown(std::net::Shutdown::Both),
+            "close socket",
+        )
+        .await
+    }
+}
+
+#[cfg(feature = "tokio-vsock")]
+impl Socket for tokio_vsock::VsockStream {
+    type ReadHalf = tokio_vsock::ReadHalf;
+    type WriteHalf = tokio_vsock::WriteHalf;
+
+    fn split(self) -> Split<Self::ReadHalf, Self::WriteHalf> {
+        let (read, write) = self.split();
+
+        Split { read, write }
+    }
+}
+
+#[cfg(feature = "tokio-vsock")]
+#[async_trait::async_trait]
+impl super::ReadHalf for tokio_vsock::ReadHalf {
+    async fn recvmsg(&mut self, buf: &mut [u8]) -> super::RecvmsgResult {
+        use tokio::io::{AsyncReadExt, ReadBuf};
+
+        let mut read_buf = ReadBuf::new(buf);
+        self.read_buf(&mut read_buf).await.map(|_| {
+            let ret = read_buf.filled().len();
+            #[cfg(unix)]
+            let ret = (ret, vec![]);
+
+            ret
+        })
+    }
+}
+
+#[cfg(feature = "tokio-vsock")]
+#[async_trait::async_trait]
+impl super::WriteHalf for tokio_vsock::WriteHalf {
+    async fn sendmsg(
+        &mut self,
+        buf: &[u8],
+        #[cfg(unix)] fds: &[std::os::unix::io::RawFd],
+    ) -> std::io::Result<usize> {
+        use std::io;
+        use tokio::io::AsyncWriteExt;
+
+        #[cfg(unix)]
+        if !fds.is_empty() {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "fds cannot be sent with a vsock stream",
+            ));
+        }
+
+        self.write(buf).await
+    }
+
+    async fn close(&mut self) -> std::io::Result<()> {
+        tokio::io::AsyncWriteExt::shutdown(self).await
+    }
+}

zbus/src/connection/socket/mod.rs
@@ -3,6 +3,7 @@ pub use split::Split;
 
 mod tcp;
 mod unix;
+mod vsock;
 
 #[cfg(not(feature = "tokio"))]
 use async_io::Async;
@@ -176,98 +177,5 @@ where
             read: arc.clone(),
             write: arc,
         }
-    }
-}
-
-#[cfg(all(feature = "vsock", not(feature = "tokio")))]
-#[async_trait::async_trait]
-impl ReadHalf for Arc<Async<vsock::VsockStream>> {
-    async fn recvmsg(&mut self, buf: &mut [u8]) -> RecvmsgResult {
-        match futures_util::AsyncReadExt::read(&mut self.as_ref(), buf).await {
-            Err(e) => Err(e),
-            Ok(len) => {
-                #[cfg(unix)]
-                let ret = (len, vec![]);
-                #[cfg(not(unix))]
-                let ret = len;
-                Ok(ret)
-            }
-        }
-    }
-}
-
-#[cfg(all(feature = "vsock", not(feature = "tokio")))]
-#[async_trait::async_trait]
-impl WriteHalf for Arc<Async<vsock::VsockStream>> {
-    async fn sendmsg(&mut self, buf: &[u8], #[cfg(unix)] fds: &[RawFd]) -> io::Result<usize> {
-        #[cfg(unix)]
-        if !fds.is_empty() {
-            return Err(io::Error::new(
-                io::ErrorKind::InvalidInput,
-                "fds cannot be sent with a vsock stream",
-            ));
-        }
-
-        futures_util::AsyncWriteExt::write(&mut self.as_ref(), buf).await
-    }
-
-    async fn close(&self) -> io::Result<()> {
-        let stream = self.clone();
-        crate::Task::spawn_blocking(
-            move || stream.get_ref().shutdown(std::net::Shutdown::Both),
-            "close socket",
-        )
-        .await
-    }
-}
-
-#[cfg(feature = "tokio-vsock")]
-impl Socket for tokio_vsock::VsockStream {
-    type ReadHalf = tokio_vsock::ReadHalf;
-    type WriteHalf = tokio_vsock::WriteHalf;
-
-    fn split(self) -> Split<Self::ReadHalf, Self::WriteHalf> {
-        let (read, write) = self.split();
-
-        Split { read, write }
-    }
-}
-
-#[cfg(feature = "tokio-vsock")]
-#[async_trait::async_trait]
-impl ReadHalf for tokio_vsock::ReadHalf {
-    async fn recvmsg(&mut self, buf: &mut [u8]) -> RecvmsgResult {
-        use tokio::io::{AsyncReadExt, ReadBuf};
-
-        let mut read_buf = ReadBuf::new(buf);
-        self.read_buf(&mut read_buf).await.map(|_| {
-            let ret = read_buf.filled().len();
-            #[cfg(unix)]
-            let ret = (ret, vec![]);
-
-            ret
-        })
-    }
-}
-
-#[cfg(feature = "tokio-vsock")]
-#[async_trait::async_trait]
-impl WriteHalf for tokio_vsock::WriteHalf {
-    async fn sendmsg(&mut self, buf: &[u8], #[cfg(unix)] fds: &[RawFd]) -> io::Result<usize> {
-        use tokio::io::AsyncWriteExt;
-
-        #[cfg(unix)]
-        if !fds.is_empty() {
-            return Err(io::Error::new(
-                io::ErrorKind::InvalidInput,
-                "fds cannot be sent with a vsock stream",
-            ));
-        }
-
-        self.write(buf).await
-    }
-
-    async fn close(&mut self) -> io::Result<()> {
-        tokio::io::AsyncWriteExt::shutdown(self).await
     }
 }

commit b68df2055ec695f0b73d40f1865aa3069a749a87

🐛 zb: Fix vsock impl of socket::WriteHalf
This fixes a regression from d137afdeabeefb552f754cdba301444477b4757f.
We really need to add vsock build (can't really test easily) to CI.

zbus/src/connection/socket/vsock.rs
@@ -39,7 +39,7 @@ impl super::WriteHalf for std::sync::Arc<async_io::Async<vsock::VsockStream>> {
         futures_util::AsyncWriteExt::write(&mut self.as_ref(), buf).await
     }
 
-    async fn close(&self) -> io::Result<()> {
+    async fn close(&mut self) -> std::io::Result<()> {
         let stream = self.clone();
         crate::Task::spawn_blocking(
             move || stream.get_ref().shutdown(std::net::Shutdown::Both),