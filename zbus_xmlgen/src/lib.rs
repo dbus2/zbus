a02b45e9c7bfa6544ff719259b4ac554017a577b
Zeeshan Ali Khan <zeenix@gmail.com>
2024-02-05 14:38:59 +0100

    📝 zx: More direct link to D-Bus standard interfaces

--- zbus_xmlgen/src/lib.rs
+++ zbus_xmlgen/src/lib.rs
@@ -123,7 +123,7 @@ fn write_doc_header<W: std::fmt::Write>(
         w,
         "//!
         //! [Writing a client proxy]: https://dbus2.github.io/zbus/client.html
-        //! [D-Bus standard interfaces]: https://dbus.freedesktop.org/doc/dbus-specification.html,
+        //! [D-Bus standard interfaces]: https://dbus.freedesktop.org/doc/dbus-specification.html#standard-interfaces,
         use zbus::proxy;
         "
     )?;
