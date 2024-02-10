a259d628eb48621523fe197fa529515310f2c875
Zeeshan Ali Khan <zeenix@gmail.com>
2024-02-10 19:10:39 +0100

    🔥 zv: Remove now unneeded lint ignore

--- zvariant/src/fd.rs
+++ zvariant/src/fd.rs
@@ -140,7 +140,6 @@ impl<'de> Deserialize<'de> for Fd<'de> {
     }
 }
 
-#[allow(clippy::unconditional_recursion)]
 impl PartialEq for Fd<'_> {
     fn eq(&self, other: &Self) -> bool {
         self.as_raw_fd().eq(&other.as_raw_fd())
