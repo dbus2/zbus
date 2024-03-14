d7220e49453a85a0ff77826265674d71f0fb6c6d
Marc-André Lureau <marcandre.lureau@redhat.com>
2024-03-14 16:00:48 +0400

    ♻️  zb: drop the return value from dispatch_call()
    It doesn't rely on Ok(false) for unhandled case, but simply on Err.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- zbus/src/object_server/mod.rs
+++ zbus/src/object_server/mod.rs
@@ -742,9 +742,9 @@ impl ObjectServer {
     /// - returning a message (responding to the caller with either a return or error message) to
     ///   the caller through the associated server connection.
     ///
-    /// Returns an error if the message is malformed, true if it's handled, false otherwise.
+    /// Returns an error if the message is malformed.
     #[instrument(skip(self))]
-    pub(crate) async fn dispatch_call(&self, msg: &Message) -> Result<bool> {
+    pub(crate) async fn dispatch_call(&self, msg: &Message) -> Result<()> {
         let conn = self.connection();
 
         if let Err(e) = self.dispatch_method_call_try(&conn, msg).await {
@@ -754,7 +754,7 @@ impl ObjectServer {
         }
         trace!("Handled: {}", msg);
 
-        Ok(true)
+        Ok(())
     }
 
     pub(crate) fn connection(&self) -> Connection {
