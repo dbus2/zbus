dbus2/zbus

commit 5a6cdd00a8c19aabd32e25bb53a79c7d503078c6

✨ Introducing connection::socket::Channel
This new socket type, uses channels for in-process p2p communication.
This can be used with `connection::Builder::authenticated_socket` to
create a connection for in-process communication.

Fixes #591.

zbus/src/connection/socket/channel.rs
@@ -1,0 +1,129 @@
+use std::io;
+
+use async_broadcast::{broadcast, Receiver, Sender};
+
+use crate::{fdo::ConnectionCredentials, Message};
+
+/// An in-process channel-based socket.
+///
+/// This is a pair of two cross-wired channels. Since all communication happens in-process, there is
+/// no need for any authentication.
+///
+/// This type is only available when `p2p` feature is enabled.
+#[derive(Debug)]
+pub struct Channel {
+    writer: Writer,
+    reader: Reader,
+}
+
+impl Channel {
+    /// Create a pair of cross-wired channels.
+    ///
+    /// Use [`crate::connection::Builder::authenticated_socket`] to create `Connection` instances
+    /// from each channel.
+    pub fn pair() -> (Self, Self) {
+        let (tx1, rx1) = broadcast(CHANNEL_CAPACITY);
+        let (tx2, rx2) = broadcast(CHANNEL_CAPACITY);
+
+        (
+            Self {
+                writer: Writer(tx1),
+                reader: Reader(rx2),
+            },
+            Self {
+                writer: Writer(tx2),
+                reader: Reader(rx1),
+            },
+        )
+    }
+}
+
+impl super::Socket for Channel {
+    type ReadHalf = Reader;
+    type WriteHalf = Writer;
+
+    fn split(self) -> super::Split<Self::ReadHalf, Self::WriteHalf> {
+        super::Split {
+            read: self.reader,
+            write: self.writer,
+        }
+    }
+}
+
+/// The reader half of a [`Channel`].
+///
+/// This type is only available when `p2p` feature is enabled.
+#[derive(Debug)]
+pub struct Reader(Receiver<Message>);
+
+#[async_trait::async_trait]
+impl super::ReadHalf for Reader {
+    async fn receive_message(
+        &mut self,
+        _seq: u64,
+        _already_received_bytes: Option<Vec<u8>>,
+    ) -> crate::Result<Message> {
+        self.0.recv().await.map_err(|e| {
+            crate::Error::InputOutput(io::Error::new(io::ErrorKind::BrokenPipe, e).into())
+        })
+    }
+
+    async fn peer_credentials(&mut self) -> io::Result<ConnectionCredentials> {
+        self_credentials().await
+    }
+}
+
+/// The writer half of a [`Channel`].
+///
+/// This type is only available when `p2p` feature is enabled.
+#[derive(Debug)]
+pub struct Writer(Sender<Message>);
+
+#[async_trait::async_trait]
+impl super::WriteHalf for Writer {
+    async fn send_message(&mut self, msg: &Message) -> crate::Result<()> {
+        self.0
+            .broadcast_direct(msg.clone())
+            .await
+            .map_err(|e| {
+                crate::Error::InputOutput(io::Error::new(io::ErrorKind::BrokenPipe, e).into())
+            })
+            .map(|removed| {
+                // We don't enable `overflow` mode so items should never be removed.
+                assert!(removed.is_none());
+            })
+    }
+
+    async fn close(&mut self) -> io::Result<()> {
+        self.0.close();
+
+        Ok(())
+    }
+
+    async fn peer_credentials(&mut self) -> io::Result<ConnectionCredentials> {
+        self_credentials().await
+    }
+}
+
+/// Return the credentials of the current process.
+async fn self_credentials() -> io::Result<ConnectionCredentials> {
+    let mut creds = ConnectionCredentials::default().set_process_id(std::process::id());
+
+    #[cfg(unix)]
+    {
+        use nix::unistd::{Gid, Uid};
+
+        creds = creds
+            .set_unix_user_id(Uid::effective().into())
+            .add_unix_group_id(Gid::effective().into());
+    }
+    #[cfg(windows)]
+    {
+        let sid = crate::win32::ProcessToken::open(None)?.sid()?;
+        creds = creds.set_windows_session_id(sid);
+    }
+
+    Ok(creds)
+}
+
+const CHANNEL_CAPACITY: usize = 32;

zbus/src/connection/mod.rs
@@ -1691,4 +1691,37 @@ mod p2p_tests {
         )
         .map(|_| ())
     }
+
+    #[test]
+    #[timeout(15000)]
+    fn channel_pair() {
+        crate::utils::block_on(test_channel_pair()).unwrap();
+    }
+
+    async fn test_channel_pair() -> Result<()> {
+        let (server1, client1) = create_channel_pair().await;
+        let (server2, client2) = create_channel_pair().await;
+
+        test_p2p(server1, client1, server2, client2).await
+    }
+
+    async fn create_channel_pair() -> (Connection, Connection) {
+        let (a, b) = socket::Channel::pair();
+
+        let guid = crate::Guid::generate();
+        let conn1 = Builder::authenticated_socket(a, guid.clone())
+            .unwrap()
+            .p2p()
+            .build()
+            .await
+            .unwrap();
+        let conn2 = Builder::authenticated_socket(b, guid)
+            .unwrap()
+            .p2p()
+            .build()
+            .await
+            .unwrap();
+
+        (conn1, conn2)
+    }
 }

zbus/src/connection/socket/mod.rs
@@ -1,3 +1,8 @@
+#[cfg(feature = "p2p")]
+pub mod channel;
+#[cfg(feature = "p2p")]
+pub use channel::Channel;
+
 mod split;
 pub use split::{BoxedSplit, Split};
 

commit f3a8f9cdc122930f9bec2095bc8d21950b43bf37

🚑️ zb: Fix build on Windows with p2p enabled
This fixes a regression from 5a6cdd00a8c19aabd32e25bb53a79c7d503078c6
where we used the wrong method name.

zbus/src/connection/socket/channel.rs
@@ -120,7 +120,7 @@ async fn self_credentials() -> io::Result<ConnectionCredentials> {
     #[cfg(windows)]
     {
         let sid = crate::win32::ProcessToken::open(None)?.sid()?;
-        creds = creds.set_windows_session_id(sid);
+        creds = creds.set_windows_sid(sid);
     }
 
     Ok(creds)