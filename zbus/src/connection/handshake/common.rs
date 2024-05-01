dbus2/zbus

commit edc79a338b7414bd33d02065302c0793372708d9

⚡️ zb: Send & receive handshake null byte along with the commands
Except in case of FreeBSD and DragonFly, the null bytes goes on the
socket as a normal byte, so there is no point of making separate
syscalls for sending and receiving null bytes.

zbus/src/connection/handshake/common.rs
@@ -12,6 +12,7 @@ pub(super) struct Common {
     cap_unix_fd: bool,
     // the current AUTH mechanism is front, ordered by priority
     mechanisms: VecDeque<AuthMechanism>,
+    first_command: bool,
 }
 
 impl Common {
@@ -22,6 +23,7 @@ impl Common {
             recv_buffer: Vec::new(),
             cap_unix_fd: false,
             mechanisms,
+            first_command: true,
         }
     }
 
@@ -68,6 +70,13 @@ impl Common {
                 .iter()
                 .map(Vec::<u8>::from)
                 .fold(vec![], |mut acc, mut c| {
+                    if self.first_command {
+                        // The first command is sent by the client so we can assume it's the client.
+                        self.first_command = false;
+                        // leading 0 is sent separately for `freebsd` and `dragonfly`.
+                        #[cfg(not(any(target_os = "freebsd", target_os = "dragonfly")))]
+                        acc.push(b'\0');
+                    }
                     acc.append(&mut c);
                     acc.extend_from_slice(b"\r\n");
                     acc
@@ -108,8 +117,26 @@ impl Common {
                     return Err(Error::Handshake("Invalid line ending in handshake".into()));
                 }
 
+                #[allow(unused_mut)]
+                let mut start_index = 0;
+                if self.first_command {
+                    // The first command is sent by the client so we can assume it's the server.
+                    self.first_command = false;
+                    // leading 0 is sent separately for `freebsd` and `dragonfly`.
+                    #[cfg(not(any(target_os = "freebsd", target_os = "dragonfly")))]
+                    {
+                        if self.recv_buffer[0] != b'\0' {
+                            return Err(Error::Handshake(
+                                "First client byte is not NUL!".to_string(),
+                            ));
+                        }
+
+                        start_index = 1;
+                    }
+                };
+
                 let line_bytes = self.recv_buffer.drain(..=lf_index);
-                let line = std::str::from_utf8(line_bytes.as_slice())
+                let line = std::str::from_utf8(&line_bytes.as_slice()[start_index..])
                     .map_err(|e| Error::Handshake(e.to_string()))?;
 
                 trace!("Reading {line}");

zbus/src/connection/handshake/client.rs
@@ -90,11 +90,11 @@ impl Client {
         Ok(())
     }
 
+    // The dbus daemon on some platforms requires sending the zero byte as a
+    // separate message with SCM_CREDS.
     #[instrument(skip(self))]
+    #[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
     async fn send_zero_byte(&mut self) -> Result<()> {
-        // The dbus daemon on some platforms requires sending the zero byte as a
-        // separate message with SCM_CREDS.
-        #[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
         let written = self
             .common
             .socket_mut()
@@ -110,19 +110,6 @@ impl Client {
                 )),
                 Some(n) => Ok(n),
             })?;
-
-        // leading 0 is sent separately already for `freebsd` and `dragonfly` above.
-        #[cfg(not(any(target_os = "freebsd", target_os = "dragonfly")))]
-        let written = self
-            .common
-            .socket_mut()
-            .write_mut()
-            .sendmsg(
-                &[b'\0'],
-                #[cfg(unix)]
-                &[],
-            )
-            .await?;
 
         if written != 1 {
             return Err(Error::Handshake(
@@ -247,6 +234,7 @@ impl Handshake for Client {
     async fn perform(mut self) -> Result<Authenticated> {
         trace!("Initializing");
 
+        #[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
         self.send_zero_byte().await?;
 
         let challenge_response = self.authenticate().await?;

zbus/src/connection/handshake/server.rs
@@ -16,7 +16,6 @@ use super::{
 #[derive(Debug)]
 #[allow(clippy::upper_case_acronyms)]
 enum ServerHandshakeStep {
-    WaitingForNull,
     WaitingForAuth,
     WaitingForData(AuthMechanism),
     WaitingForBegin,
@@ -63,7 +62,7 @@ impl<'s> Server<'s> {
 
         Ok(Server {
             common: Common::new(socket, mechanisms),
-            step: ServerHandshakeStep::WaitingForNull,
+            step: ServerHandshakeStep::WaitingForAuth,
             #[cfg(unix)]
             client_uid,
             #[cfg(windows)]
@@ -182,27 +181,6 @@ impl<'s> Server<'s> {
     #[instrument(skip(self))]
     async fn next_step(&mut self) -> Result<bool> {
         match self.step {
-            ServerHandshakeStep::WaitingForNull => {
-                trace!("Waiting for NULL");
-                let mut buffer = [0; 1];
-                let read = self
-                    .common
-                    .socket_mut()
-                    .read_mut()
-                    .recvmsg(&mut buffer)
-                    .await?;
-                #[cfg(unix)]
-                let read = read.0;
-                // recvmsg cannot return anything else than Ok(1) or Err
-                debug_assert!(read == 1);
-                if buffer[0] != 0 {
-                    return Err(Error::Handshake(
-                        "First client byte is not NUL!".to_string(),
-                    ));
-                }
-                trace!("Received NULL from client");
-                self.step = ServerHandshakeStep::WaitingForAuth;
-            }
             ServerHandshakeStep::WaitingForAuth => {
                 trace!("Waiting for authentication");
                 let reply = self.common.read_command().await?;

commit 4d53e77f47a4f3928edb0a7b8f14716af66d732a

🔌 zb: Always parse 0 byte in server handshake
This fixes a regression from edc79a338b7414bd33d02065302c0793372708d9.

Fixes #756.

zbus/src/connection/handshake/common.rs
@@ -122,17 +122,13 @@ impl Common {
                 if self.first_command {
                     // The first command is sent by the client so we can assume it's the server.
                     self.first_command = false;
-                    // leading 0 is sent separately for `freebsd` and `dragonfly`.
-                    #[cfg(not(any(target_os = "freebsd", target_os = "dragonfly")))]
-                    {
-                        if self.recv_buffer[0] != b'\0' {
-                            return Err(Error::Handshake(
-                                "First client byte is not NUL!".to_string(),
-                            ));
-                        }
-
-                        start_index = 1;
+                    if self.recv_buffer[0] != b'\0' {
+                        return Err(Error::Handshake(
+                            "First client byte is not NUL!".to_string(),
+                        ));
                     }
+
+                    start_index = 1;
                 };
 
                 let line_bytes = self.recv_buffer.drain(..=lf_index);