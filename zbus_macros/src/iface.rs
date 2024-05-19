dbus2/zbus

commit 91abff76a964d9c2ba7907435886ee9810395d02

✨ zm: add `spawn` attribute on interface
Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

zbus_macros/src/iface.rs
@@ -18,7 +18,8 @@ pub mod old {
 
         pub TraitAttributes("trait") {
             interface str,
-            name str
+            name str,
+            spawn bool
         };
 
         pub MethodAttributes("method") {
@@ -39,7 +40,8 @@ def_attrs! {
 
     pub TraitAttributes("trait") {
         interface str,
-        name str
+        name str,
+        spawn bool
     };
 
     pub MethodAttributes("method") {
@@ -300,13 +302,13 @@ pub fn expand<T: AttrParse + Into<TraitAttrs>, M: AttrParse + Into<MethodAttrs>>
         _ => return Err(Error::new_spanned(&input.self_ty, "Invalid type")),
     };
 
-    let iface_name =
-        {
-            let (name, interface) = match T::parse_nested_metas(&args)?.into() {
-                TraitAttrs::New(new) => (new.name, new.interface),
-                TraitAttrs::Old(old) => (old.name, old.interface),
-            };
+    let (iface_name, with_spawn) = {
+        let (name, interface, spawn) = match T::parse_nested_metas(&args)?.into() {
+            TraitAttrs::New(new) => (new.name, new.interface, new.spawn),
+            TraitAttrs::Old(old) => (old.name, old.interface, old.spawn),
+        };
 
+        let name =
             match (name, interface) {
                 (Some(name), None) | (None, Some(name)) => name,
                 (None, None) => format!("org.freedesktop.{ty}"),
@@ -314,8 +316,10 @@ pub fn expand<T: AttrParse + Into<TraitAttrs>, M: AttrParse + Into<MethodAttrs>>
                     input.span(),
                     "`name` and `interface` attributes should not be specified at the same time",
                 )),
-            }
-        };
+            };
+
+        (name, !spawn.unwrap_or(true))
+    };
 
     // Store parsed information about each method
     let mut methods = vec![];
@@ -711,6 +715,10 @@ pub fn expand<T: AttrParse + Into<TraitAttrs>, M: AttrParse + Into<MethodAttrs>>
         {
             fn name() -> #zbus::names::InterfaceName<'static> {
                 #zbus::names::InterfaceName::from_static_str_unchecked(#iface_name)
+            }
+
+            fn spawn_tasks_for_methods(&self) -> bool {
+                #with_spawn
             }
 
             async fn get(

zbus_macros/src/lib.rs
@@ -215,6 +215,25 @@ pub fn dbus_proxy(attr: TokenStream, item: TokenStream) -> TokenStream {
 /// properties or signal depending on the item attributes. It will implement the [`Interface`] trait
 /// `for T` on your behalf, to handle the message dispatching and introspection support.
 ///
+/// The trait accepts the `interface` attributes:
+///
+/// * `name` - the D-Bus interface name
+///
+/// * `spawn` - Controls the spawning of tasks for method calls. By default, `true`, allowing zbus
+///   to spawn a separate task for each method call. This default behavior can lead to methods being
+///   handled out of their received order, which might not always align with expected or desired
+///   behavior.
+///
+///   - **When True (Default):** Suitable for interfaces where method calls are independent of each
+///   other or can be processed asynchronously without strict ordering. In scenarios where a client
+///   must wait for a reply before making further dependent calls, this default behavior is
+///   appropriate.
+///
+///   - **When False:** Use this setting to ensure methods are handled in the order they are
+///   received, which is crucial for interfaces requiring sequential processing of method calls.
+///   However, care must be taken to avoid making D-Bus method calls from within your interface
+///   methods when this setting is false, as it may lead to deadlocks under certain conditions.
+///
 /// The methods accepts the `interface` attributes:
 ///
 /// * `name` - override the D-Bus name (pascal case form of the method by default)

zbus_macros/tests/tests.rs
@@ -139,7 +139,7 @@ fn test_interface() {
     #[derive(Serialize, Deserialize, Type, Value)]
     struct MyCustomPropertyType(u32);
 
-    #[interface(name = "org.freedesktop.zbus.Test")]
+    #[interface(name = "org.freedesktop.zbus.Test", spawn = false)]
     impl<T: 'static> Test<T>
     where
         T: serde::ser::Serialize + zbus::zvariant::Type + Send + Sync,

commit 0b22737c68355df63635793bc9314d6040dc7801

🚑️ zm: Fix default value of `Interface::spawn_task_for_method`
This fixes a regression introduced by
91abff76a964d9c2ba7907435886ee9810395d02, resulting in tasks not being
spawned for methods by default.

Fixes #799.

zbus_macros/src/iface.rs
@@ -327,7 +327,7 @@ pub fn expand<T: AttrParse + Into<TraitAttrs>, M: AttrParse + Into<MethodAttrs>>
                 )),
             };
 
-        (name, !spawn.unwrap_or(true))
+        (name, !spawn.unwrap_or(false))
     };
 
     // Store parsed information about each method