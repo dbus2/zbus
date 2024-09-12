dac3f82bfc736e8f6185f05309d26417a2a1524f
Valentin Hăloiu <valentin.haloiu@gmail.com>
2024-09-12 22:06:16 +0100

    ♻️  zb: Enable process abstraction for all `unix` targets
    The `process` abstraction will be used (on `unix` targets)
    for spawning child processes when implementing the
    `unixexec` D-Bus transport.

--- zbus/src/abstractions/process.rs
+++ zbus/src/abstractions/process.rs
@@ -1,6 +1,8 @@
+#[cfg(target_os = "macos")]
 use std::{ffi::OsStr, io::Error, process::Output};
 
 /// An asynchronous wrapper around running and getting command output
+#[cfg(target_os = "macos")]
 pub async fn run<I, S>(program: S, args: I) -> Result<Output, Error>
 where
     I: IntoIterator<Item = S>,
