fa49f62955eea655feb3cbcd87f01920addf22dd
Zeeshan Ali Khan <zeenix@gmail.com>
2024-09-22 16:46:55 +0200

    🔥 zb: Don't repeat the comment

--- zbus/tests/issue/mod.rs
+++ zbus/tests/issue/mod.rs
@@ -8,11 +8,9 @@ mod issue_68;
 mod issue_799;
 mod issue_81;
 
-// Issue specific to tokio runtime.
+// Issues specific to tokio runtime.
 #[cfg(all(unix, feature = "tokio", feature = "p2p"))]
 mod issue_279;
-
-// Issue specific to tokio runtime.
 #[cfg(all(unix, feature = "tokio"))]
 mod issue_310;
 
