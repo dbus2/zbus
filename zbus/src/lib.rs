bba214428d0faab0bb8cd7b395e9c0d92ce29cf2
Zeeshan Ali Khan <zeenix@gmail.com>
2024-09-29 18:25:47 +0200

    🔥 zb: Drop all deprecated API from crate root

--- zbus/src/lib.rs
+++ zbus/src/lib.rs
@@ -55,38 +55,10 @@ pub use guid::*;
 pub mod message;
 pub use message::Message;
 
-#[deprecated(since = "4.0.0", note = "Use `message::Builder` instead")]
-#[doc(hidden)]
-pub use message::Builder as MessageBuilder;
-#[deprecated(since = "4.0.0", note = "Use `message::EndianSig` instead")]
-#[doc(hidden)]
-pub use message::EndianSig;
-#[doc(hidden)]
-pub use message::Flags as MessageFlags;
-#[deprecated(since = "4.0.0", note = "Use `message::Header` instead")]
-#[doc(hidden)]
-pub use message::Header as MessageHeader;
-#[deprecated(since = "4.0.0", note = "Use `message::PrimaryHeader` instead")]
-#[doc(hidden)]
-pub use message::PrimaryHeader as MessagePrimaryHeader;
-#[deprecated(since = "4.0.0", note = "Use `message::Sequence` instead")]
-#[doc(hidden)]
-pub use message::Sequence as MessageSequence;
-#[deprecated(since = "4.0.0", note = "Use `message::Type` instead")]
-#[doc(hidden)]
-pub use message::Type as MessageType;
-#[deprecated(since = "4.0.0", note = "Use `message::NATIVE_ENDIAN_SIG` instead")]
-#[doc(hidden)]
-pub use message::NATIVE_ENDIAN_SIG;
-
 pub mod connection;
 /// Alias for `connection` module, for convenience.
 pub use connection as conn;
 pub use connection::{handshake::AuthMechanism, Connection};
-
-#[deprecated(since = "4.0.0", note = "Use `connection::Builder` instead")]
-#[doc(hidden)]
-pub use connection::Builder as ConnectionBuilder;
 
 mod message_stream;
 pub use message_stream::*;
@@ -96,78 +68,17 @@ pub use abstractions::*;
 pub mod match_rule;
 pub use match_rule::{MatchRule, OwnedMatchRule};
 
-#[deprecated(since = "4.0.0", note = "Use `match_rule::Builder` instead")]
-#[doc(hidden)]
-pub use match_rule::Builder as MatchRuleBuilder;
-#[deprecated(since = "4.0.0", note = "Use `match_rule::PathSpec` instead")]
-#[doc(hidden)]
-pub use match_rule::PathSpec as MatchRulePathSpec;
-
 pub mod proxy;
 pub use proxy::Proxy;
 
-#[deprecated(since = "4.0.0", note = "Use `proxy::Builder` instead")]
-#[doc(hidden)]
-pub use proxy::Builder as ProxyBuilder;
-#[deprecated(since = "4.0.0", note = "Use `proxy::CacheProperties` instead")]
-#[doc(hidden)]
-pub use proxy::CacheProperties;
-#[deprecated(since = "4.0.0", note = "Use `proxy::MethodFlags` instead")]
-#[doc(hidden)]
-pub use proxy::MethodFlags;
-#[deprecated(since = "4.0.0", note = "Use `proxy::OwnerChangedStream` instead")]
-#[doc(hidden)]
-pub use proxy::OwnerChangedStream;
-#[deprecated(since = "4.0.0", note = "Use `proxy::PropertyChanged` instead")]
-#[doc(hidden)]
-pub use proxy::PropertyChanged;
-#[deprecated(since = "4.0.0", note = "Use `proxy::PropertyStream` instead")]
-#[doc(hidden)]
-pub use proxy::PropertyStream;
-#[deprecated(since = "4.0.0", note = "Use `proxy::ProxyDefault` instead")]
-#[doc(hidden)]
-pub use proxy::ProxyDefault;
-
 pub mod object_server;
 pub use object_server::ObjectServer;
-
-#[deprecated(since = "4.0.0", note = "Use `object_server::DispatchResult` instead")]
-#[doc(hidden)]
-pub use object_server::DispatchResult;
-#[deprecated(since = "4.0.0", note = "Use `object_server::Interface` instead")]
-#[doc(hidden)]
-pub use object_server::Interface;
-#[deprecated(since = "4.0.0", note = "Use `object_server::InterfaceDeref` instead")]
-#[doc(hidden)]
-pub use object_server::InterfaceDeref;
-#[deprecated(
-    since = "4.0.0",
-    note = "Use `object_server::InterfaceDerefMut` instead"
-)]
-#[doc(hidden)]
-pub use object_server::InterfaceDerefMut;
-#[deprecated(since = "4.0.0", note = "Use `object_server::InterfaceRef` instead")]
-#[doc(hidden)]
-pub use object_server::InterfaceRef;
-#[deprecated(
-    since = "4.0.0",
-    note = "Use `object_server::ResponseDispatchNotifier` instead"
-)]
-#[doc(hidden)]
-pub use object_server::ResponseDispatchNotifier;
-#[deprecated(since = "4.0.0", note = "Use `object_server::SignalContext` instead")]
-#[doc(hidden)]
-pub use object_server::SignalContext;
 
 mod utils;
 pub use utils::*;
 
 #[macro_use]
 pub mod fdo;
-
-#[deprecated(since = "4.0.0", note = "Use `connection::Socket` instead")]
-#[doc(hidden)]
-pub use connection::Socket;
 
 pub mod blocking;
 
