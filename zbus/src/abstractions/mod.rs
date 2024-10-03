6b2d173e68e82d1197c5d4a45a6dffca2988e69d
Zeeshan Ali Khan <zeenix@gmail.com>
2024-10-03 22:19:17 +0200

    🔥 zb: Drop now unused internal API

--- zbus/src/abstractions/mod.rs
+++ zbus/src/abstractions/mod.rs
@@ -7,7 +7,6 @@ pub use executor::*;
 mod async_drop;
 pub(crate) mod async_lock;
 pub use async_drop::*;
-pub(crate) mod file;
 
 // Not macOS-specific itself but only used on macOS.
 #[cfg(target_os = "macos")]
