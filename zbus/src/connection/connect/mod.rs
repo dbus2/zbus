f7505e8d01e53b80b6e489109235309d36c141ce
Zeeshan Ali Khan <zeenix@gmail.com>
2024-10-05 20:41:26 +0200

    ⚡️ zb: Remove unnecessary boxing of a future

--- zbus/src/connection/connect/mod.rs
+++ zbus/src/connection/connect/mod.rs
@@ -1,4 +1,3 @@
-use std::{future::Future, pin::Pin};
 use tracing::debug;
 
 use crate::{
@@ -28,37 +27,33 @@ pub(crate) async fn connect_address(
     Err(Error::Address("No connectable address".into()))
 }
 
-fn connect(
-    addr: &Address<'_>,
-) -> Pin<Box<dyn Future<Output = ConnectResult> + Send + Sync + 'static>> {
+async fn connect(addr: &Address<'_>) -> ConnectResult {
     let addr = addr.to_owned();
-    Box::pin(async move {
-        let guid = match addr.guid() {
-            Some(g) => Some(Guid::try_from(g.as_ref())?.into()),
-            _ => None,
-        };
-        let split = match addr.transport()? {
-            Transport::Tcp(t) => socket::tcp::connect(&t).await?.into(),
-            Transport::NonceTcp(t) => socket::tcp::connect_nonce(&t).await?.into(),
-            #[cfg(any(unix, not(feature = "tokio")))]
-            Transport::Unix(u) => socket::unix::connect(&u).await?.into(),
-            #[cfg(any(
-                all(feature = "vsock", not(feature = "tokio")),
-                feature = "tokio-vsock"
-            ))]
-            Transport::Vsock(v) => socket::vsock::connect(&v).await?.into(),
-            #[cfg(target_os = "macos")]
-            Transport::Launchd(l) => macos::connect(&l).await?.into(),
-            #[cfg(target_os = "windows")]
-            Transport::Autolaunch(l) => {
-                return win32::connect(&l).await;
-            }
-            _ => {
-                return Err(Error::Address(format!("Unhandled address: {}", addr)));
-            }
-        };
-        Ok((split, guid))
-    })
+    let guid = match addr.guid() {
+        Some(g) => Some(Guid::try_from(g.as_ref())?.into()),
+        _ => None,
+    };
+    let split = match addr.transport()? {
+        Transport::Tcp(t) => socket::tcp::connect(&t).await?.into(),
+        Transport::NonceTcp(t) => socket::tcp::connect_nonce(&t).await?.into(),
+        #[cfg(any(unix, not(feature = "tokio")))]
+        Transport::Unix(u) => socket::unix::connect(&u).await?.into(),
+        #[cfg(any(
+            all(feature = "vsock", not(feature = "tokio")),
+            feature = "tokio-vsock"
+        ))]
+        Transport::Vsock(v) => socket::vsock::connect(&v).await?.into(),
+        #[cfg(target_os = "macos")]
+        Transport::Launchd(l) => macos::connect(&l).await?.into(),
+        #[cfg(target_os = "windows")]
+        Transport::Autolaunch(l) => {
+            return win32::connect(&l).await;
+        }
+        _ => {
+            return Err(Error::Address(format!("Unhandled address: {}", addr)));
+        }
+    };
+    Ok((split, guid))
 }
 
 type ConnectResult = Result<(BoxedSplit, Option<OwnedGuid>)>;
