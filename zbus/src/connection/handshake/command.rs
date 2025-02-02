dbus2/zbus

commit b792340fc31232e6eced6af99f53bc98e991353a

🛂 zb: Use string type for auth mechanisms in Rejected cmd
Now that we don't support all authentication mechanisms described in the
spec, we'll get a parse error on receiving an unsupported mechanism.
Let's use the raw string type for the mechanism field in the Rejected
command to avoid this.

We do so in a way that limits allocations, especially on the
server-side, which might need to reject a lot of connections and it will
have more impact there.

zbus/src/connection/handshake/command.rs
@@ -1,4 +1,4 @@
-use std::{fmt, str::FromStr};
+use std::{borrow::Cow, fmt, str::FromStr};
 
 use crate::{conn::AuthMechanism, Error, Guid, OwnedGuid, Result};
 
@@ -15,7 +15,7 @@ pub(super) enum Command {
     Data(Option<Vec<u8>>),
     Error(String),
     NegotiateUnixFD,
-    Rejected(Vec<AuthMechanism>),
+    Rejected(Cow<'static, str>),
     Ok(OwnedGuid),
     AgreeUnixFD,
 }
@@ -42,17 +42,7 @@ impl fmt::Display for Command {
             },
             Command::Error(expl) => write!(f, "ERROR {expl}"),
             Command::NegotiateUnixFD => write!(f, "NEGOTIATE_UNIX_FD"),
-            Command::Rejected(mechs) => {
-                write!(
-                    f,
-                    "REJECTED {}",
-                    mechs
-                        .iter()
-                        .map(|m| m.to_string())
-                        .collect::<Vec<_>>()
-                        .join(" ")
-                )
-            }
+            Command::Rejected(mechs) => write!(f, "REJECTED {mechs}"),
             Command::Ok(guid) => write!(f, "OK {guid}"),
             Command::AgreeUnixFD => write!(f, "AGREE_UNIX_FD"),
         }
@@ -90,7 +80,7 @@ impl FromStr for Command {
             Some("ERROR") => Command::Error(s.into()),
             Some("NEGOTIATE_UNIX_FD") => Command::NegotiateUnixFD,
             Some("REJECTED") => {
-                let mechs = words.map(|m| m.parse()).collect::<Result<_>>()?;
+                let mechs = words.map(|s| s.to_owned()).collect();
                 Command::Rejected(mechs)
             }
             Some("OK") => {

zbus/src/connection/handshake/client.rs
@@ -96,11 +96,7 @@ impl Client {
                 Ok(())
             }
             Command::Rejected(accepted) => {
-                let list = accepted
-                    .iter()
-                    .map(|m| m.to_string())
-                    .collect::<Vec<_>>()
-                    .join(", ");
+                let list = accepted.replace(" ", ", ");
                 Err(Error::Handshake(format!(
                     "{mechanism} rejected by the server. Accepted mechanisms: [{list}]"
                 )))

zbus/src/connection/handshake/server.rs
@@ -102,7 +102,7 @@ impl Server {
 
     #[instrument(skip(self))]
     async fn rejected_error(&mut self) -> Result<()> {
-        let cmd = Command::Rejected(vec![self.common.mechanism()]);
+        let cmd = Command::Rejected(self.common.mechanism().as_str().into());
         trace!("Sending authentication error");
         self.common.write_command(cmd).await?;
         self.step = ServerHandshakeStep::WaitingForAuth;

commit 3cc4c8facdc5b53f4c3ee21cbbd71e5b6505e10c

🐛 zb: Correctly format list of accepted auth mechanisms
This fixes a regression from b792340.

Fixes #1216.

zbus/src/connection/handshake/command.rs
@@ -80,7 +80,7 @@ impl FromStr for Command {
             Some("ERROR") => Command::Error(s.into()),
             Some("NEGOTIATE_UNIX_FD") => Command::NegotiateUnixFD,
             Some("REJECTED") => {
-                let mechs = words.map(|s| s.to_owned()).collect();
+                let mechs = words.collect::<Vec<_>>().join(" ").into();
                 Command::Rejected(mechs)
             }
             Some("OK") => {