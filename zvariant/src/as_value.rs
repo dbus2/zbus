111c34ef261e68bcf74d7d98233fe444d0517b26
Zeeshan Ali Khan <zeenix@gmail.com>
2025-05-03 11:59:21 +0200

    📝 zv: Update `as_value` module docs

--- zvariant/src/as_value.rs
+++ zvariant/src/as_value.rs
@@ -1,8 +1,8 @@
 //! Utilities to (de)serialize a value as a [`enum@zvariant::Value`].
 //!
-//! Utilities to use with `serde::{Deserialize, Serialize}` to serialize and deserialize a given
-//! value as a `zvariant::Value` (variant type). See the relevant [FAQ entry] in our book for more
-//! details and examples.
+//! This is mainly useful for constructing a strongly-typed API for dealing with dictionaries
+//! containing string keys and variant values (`a{sv}` in D-Bus language) See the relevant
+//! [FAQ entry] in our book for more details and examples.
 //!
 //! [FAQ entry]: https://dbus2.github.io/zbus/faq.html#how-to-use-a-struct-as-a-dictionary
 
