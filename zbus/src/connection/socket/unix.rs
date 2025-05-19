dbus2/zbus

commit 3602dcbb3a2a1a6f89ab0b34f6f68af9053d5ae1

✨ zb: Add ProcessFD/pidfd to peer ConnectionCredentials
D-Bus supports the ProcessFD key in the GetConnectionCredentials results
which is useful for identifying the peer without races. This commit adds
to pidfd to the peer ConnectionCredentials which means that busd will
send the key.

zbus/src/connection/socket/unix.rs
@@ -336,10 +336,15 @@ fn get_unix_peer_creds_blocking(fd: RawFd) -> io::Result<crate::fdo::ConnectionC
     #[cfg(any(target_os = "android", target_os = "linux"))]
     {
         use nix::{
-            sys::socket::{getsockopt, sockopt::PeerCredentials},
+            errno::Errno,
+            sys::socket::{
+                getsockopt,
+                sockopt::{PeerCredentials, PeerPidfd},
+            },
             unistd::{getgrouplist, Gid, Uid, User},
         };
         use std::ffi::CString;
+        use zvariant::OwnedFd;
 
         let (uid, gid, pid) = {
             let unix_creds = getsockopt(&fd, PeerCredentials)?;
@@ -366,6 +371,12 @@ fn get_unix_peer_creds_blocking(fd: RawFd) -> io::Result<crate::fdo::ConnectionC
         for group in groups.iter() {
             creds = creds.add_unix_group_id(group.as_raw());
         }
+
+        match getsockopt(&fd, PeerPidfd) {
+            Err(Errno::ENOPROTOOPT) => (),
+            Ok(pidfd) => creds = creds.set_process_fd(OwnedFd::from(pidfd)),
+            Err(e) => return Err(e.into()),
+        };
     }
 
     #[cfg(any(

commit 2d32ed346cc203509f23d53bd48cd82601717af5

🐛 zb: Fix build on Android by limiting PeerPidfd to linux
For whatever reason the nix::sys::socket::sockopt::PeerPidfd is only
available to target_os = "linux", so properly guard the code using it
against that.

Fixes: 3602dcbb ("✨ zb: Add ProcessFD/pidfd to peer ConnectionCredentials")

zbus/src/connection/socket/unix.rs
@@ -336,15 +336,10 @@ fn get_unix_peer_creds_blocking(fd: RawFd) -> io::Result<crate::fdo::ConnectionC
     #[cfg(any(target_os = "android", target_os = "linux"))]
     {
         use nix::{
-            errno::Errno,
-            sys::socket::{
-                getsockopt,
-                sockopt::{PeerCredentials, PeerPidfd},
-            },
+            sys::socket::{getsockopt, sockopt::PeerCredentials},
             unistd::{getgrouplist, Gid, Uid, User},
         };
         use std::ffi::CString;
-        use zvariant::OwnedFd;
 
         let (uid, gid, pid) = {
             let unix_creds = getsockopt(&fd, PeerCredentials)?;
@@ -372,11 +367,17 @@ fn get_unix_peer_creds_blocking(fd: RawFd) -> io::Result<crate::fdo::ConnectionC
             creds = creds.add_unix_group_id(group.as_raw());
         }
 
-        match getsockopt(&fd, PeerPidfd) {
-            Err(Errno::ENOPROTOOPT) => (),
-            Ok(pidfd) => creds = creds.set_process_fd(OwnedFd::from(pidfd)),
-            Err(e) => return Err(e.into()),
-        };
+        #[cfg(target_os = "linux")]
+        {
+            use nix::{errno::Errno, sys::socket::sockopt::PeerPidfd};
+            use zvariant::OwnedFd;
+
+            match getsockopt(&fd, PeerPidfd) {
+                Err(Errno::ENOPROTOOPT) => (),
+                Ok(pidfd) => creds = creds.set_process_fd(OwnedFd::from(pidfd)),
+                Err(e) => return Err(e.into()),
+            };
+        }
     }
 
     #[cfg(any(