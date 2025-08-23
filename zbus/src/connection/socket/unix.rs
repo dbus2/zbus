dbus2/zbus

commit c30dcdd776401354541c7362b3bcc49e4b7c992f

♻️ zb: Restructure get_unix_peer_creds for the next commits
This lets us share more code in the generic path and mutate the
ConnectionCredentials as needed.

zbus/src/connection/socket/unix.rs
@@ -331,18 +331,41 @@ fn get_unix_peer_creds_blocking(fd: RawFd) -> io::Result<crate::fdo::ConnectionC
     // TODO: get this BorrowedFd directly from get_unix_peer_creds(), but this requires a
     // 'static lifetime due to the Task.
     let fd = unsafe { BorrowedFd::borrow_raw(fd) };
+    let mut creds = crate::fdo::ConnectionCredentials::default();
 
     #[cfg(any(target_os = "android", target_os = "linux"))]
     {
-        use nix::sys::socket::{getsockopt, sockopt::PeerCredentials};
+        use nix::{
+            sys::socket::{getsockopt, sockopt::PeerCredentials},
+            unistd::{getgrouplist, Gid, Uid, User},
+        };
+        use std::ffi::CString;
 
-        getsockopt(&fd, PeerCredentials)
-            .map(|creds| {
-                crate::fdo::ConnectionCredentials::default()
-                    .set_process_id(creds.pid() as _)
-                    .set_unix_user_id(creds.uid())
-            })
-            .map_err(|e| e.into())
+        let (uid, gid, pid) = {
+            let unix_creds = getsockopt(&fd, PeerCredentials)?;
+            (
+                Uid::from_raw(unix_creds.uid()),
+                Gid::from_raw(unix_creds.uid()),
+                unix_creds.pid() as u32,
+            )
+        };
+        creds = creds.set_unix_user_id(uid.as_raw()).set_process_id(pid);
+
+        // the dbus spec requires groups to be either absent or complete (primary + secondary
+        // groups)
+        let mut groups = User::from_uid(uid)?
+            .map(|user| CString::new(user.name))
+            .transpose()?
+            .map(|user| getgrouplist(&user, gid))
+            .transpose()
+            .ok()
+            .flatten()
+            .unwrap_or(Vec::new());
+        // it also requires the groups to be numerically sorted
+        groups.sort_by_key(|gid| gid.as_raw());
+        for group in groups.iter() {
+            creds = creds.add_unix_group_id(group.as_raw());
+        }
     }
 
     #[cfg(any(
@@ -354,10 +377,13 @@ fn get_unix_peer_creds_blocking(fd: RawFd) -> io::Result<crate::fdo::ConnectionC
         target_os = "netbsd"
     ))]
     {
-        let uid = nix::unistd::getpeereid(fd).map(|(uid, _)| uid.into())?;
-        // FIXME: Handle pid fetching too.
-        Ok(crate::fdo::ConnectionCredentials::default().set_unix_user_id(uid))
+        let (uid, _gid) = nix::unistd::getpeereid(fd)?;
+        creds = creds.set_unix_user_id(uid.as_raw())
+
+        // FIXME: Handle pid fetching too
     }
+
+    Ok(creds)
 }
 
 // Send 0 byte as a separate SCM_CREDS message.

commit c5cd9ccfbb8c8639e56c2b9bc962a5c15bdf6b7c

🐛 zb: Fall back to no groups rather than erroring out for peer creds
It turns out that looking up the name of a user id requires some files
which can be absent in some sandboxes/namespaces. The dbus spec allows
us to send either no group or all groups. So instead of erroring out, we
can just do what we did before and send no groups at all in those cases.

This also adds some debug logging for the possible runtime errors to
indicate why no groups were sent.

Discovered and fix proposed by Sophie Herold.

Fixes: c30dcdd7 ("♻️ zb: Restructure get_unix_peer_creds for the next commits")

zbus/src/connection/socket/unix.rs
@@ -340,6 +340,7 @@ fn get_unix_peer_creds_blocking(fd: RawFd) -> io::Result<crate::fdo::ConnectionC
             unistd::{getgrouplist, Gid, Uid, User},
         };
         use std::ffi::CString;
+        use tracing::debug;
 
         let (uid, gid, pid) = {
             let unix_creds = getsockopt(&fd, PeerCredentials)?;
@@ -353,11 +354,15 @@ fn get_unix_peer_creds_blocking(fd: RawFd) -> io::Result<crate::fdo::ConnectionC
 
         // the dbus spec requires groups to be either absent or complete (primary + secondary
         // groups)
-        let mut groups = User::from_uid(uid)?
+        let mut groups = User::from_uid(uid)
+            .map_err(|e| debug!("User lookup failed: {}", e))
+            .ok()
+            .flatten()
             .map(|user| CString::new(user.name))
             .transpose()?
             .map(|user| getgrouplist(&user, gid))
             .transpose()
+            .map_err(|e| debug!("Group lookup failed: {}", e))
             .ok()
             .flatten()
             .unwrap_or(Vec::new());